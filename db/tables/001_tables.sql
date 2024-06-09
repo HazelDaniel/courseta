b0a534c5a07f700dd813d3abe2c02785b2c51407
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-09 22:20:10 +0100

    added a creator_pass column to creators table with a unique constraint

--- db/tables/001_tables.sql
+++ db/tables/001_tables.sql
@@ -34,7 +34,9 @@ BEGIN
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     password TEXT NOT NULL,
+    creator_pass TEXT NOT NULL DEFAULT gen_random_uuid()::TEXT,
     UNIQUE(email),
+    UNIQUE(creator_pass),
     course_review_count INT NOT NULL DEFAULT 0,
     student_count INT NOT NULL DEFAULT 0,
     role courseta.USER_ROLE_TYPE NOT NULL DEFAULT 'creator' CHECK (role = 'creator')
