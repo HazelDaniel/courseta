c32a194ac1cafa281abfae4bf4310cbe70dddfb1
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-09 17:28:54 +0100

    added password fields to students and creators

--- db/tables/001_tables.sql
+++ db/tables/001_tables.sql
@@ -23,6 +23,7 @@ BEGIN
     email VARCHAR(256) NOT NULL,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
+    password TEXT NOT NULL,
     role courseta.USER_ROLE_TYPE NOT NULL DEFAULT 'student' CHECK (role = 'student')
   );
 
@@ -31,6 +32,7 @@ BEGIN
     email VARCHAR(256) NOT NULL,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
+    password TEXT NOT NULL,
     course_review_count INT NOT NULL DEFAULT 0,
     student_count INT NOT NULL DEFAULT 0,
     role courseta.USER_ROLE_TYPE NOT NULL DEFAULT 'creator' CHECK (role = 'creator')
