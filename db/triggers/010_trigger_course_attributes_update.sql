226907d5b17bbd8e656c5731783bdee87cbe88ba
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-10 17:30:52 +0100

    added the trigger for course progress update and course quiz count update

--- db/triggers/010_trigger_course_attributes_update.sql
+++ db/triggers/010_trigger_course_attributes_update.sql
@@ -68,6 +68,38 @@ BEGIN
 
   RAISE NOTICE '<[SETUP]   TRIGGER: DONE updating course.lesson_count deduction triggers ...';
 
+
+  RAISE NOTICE '<[SETUP]   TRIGGER: updating course.progress update triggers ...';
+
+  CREATE OR REPLACE TRIGGER trigger_course_progress_update
+  AFTER INSERT
+  ON students__assessments
+  FOR EACH ROW
+  EXECUTE FUNCTION update_equiv_course_progress();
+
+  RAISE NOTICE '<[SETUP]   TRIGGER: DONE updating course.progress update triggers ...';
+
+
+  RAISE NOTICE '<[SETUP]   TRIGGER: updating course.quiz_count increment triggers ...';
+
+  CREATE OR REPLACE TRIGGER trigger_course_quiz_count_increment
+  AFTER INSERT
+  ON quizzes
+  FOR EACH ROW
+  EXECUTE FUNCTION add_quiz_count_to_course();
+
+  RAISE NOTICE '<[SETUP]   TRIGGER: DONE updating course.quiz_count increment triggers ...';
+
+  RAISE NOTICE '<[SETUP]   TRIGGER: updating course.quiz_count decrement triggers ...';
+
+  CREATE OR REPLACE TRIGGER trigger_course_quiz_count_decrement
+  AFTER DELETE
+  ON quizzes
+  FOR EACH ROW
+  EXECUTE FUNCTION remove_quiz_count_from_course();
+
+  RAISE NOTICE '<[SETUP]   TRIGGER: DONE updating course.quiz_count decrement triggers ...';
+
   RAISE NOTICE '[SETUP]   TRIGGER: DONE updating course.[attribute] update triggers ...';
 END
 $block$ LANGUAGE PLPGSQL;
