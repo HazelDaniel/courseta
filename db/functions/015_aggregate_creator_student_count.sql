6d74205d5d914e8fab32cc89c5be4af5951fb5dd
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-10 17:27:29 +0100

    changed the parameter types in the procedure 'update_student_equiv_creator_count' and returned the NEW keyword in trigger function

--- db/functions/015_aggregate_creator_student_count.sql
+++ db/functions/015_aggregate_creator_student_count.sql
@@ -3,25 +3,26 @@ $block$
 BEGIN
   RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for update of the creator.student_count.';
 
+  CREATE OR REPLACE PROCEDURE update_student_equiv_creator_count
+  (creator_id_ UUID) LANGUAGE PLPGSQL AS
+  $block2$
+  DECLARE
+    tot_student_count         INT;
+  BEGIN
+    SELECT INTO tot_student_count COALESCE(SUM(student_count), 0) FROM courses WHERE courses.creator_id = creator_id;
+    UPDATE creators SET student_count = tot_student_count WHERE creators.creator_id = creator_id_;
+  END;
+  $block2$;
+
   CREATE OR REPLACE FUNCTION update_student_count_on_creator () RETURNS TRIGGER AS
   $block1$
   BEGIN
     IF NEW.student_count <> OLD.student_count THEN
       CALL update_student_equiv_creator_count(NEW.creator_id);
     END IF;
+    RETURN NEW;
   END;
   $block1$ LANGUAGE PLPGSQL;
-
-  CREATE OR REPLACE PROCEDURE update_student_equiv_creator_count
-  (creator_id BIGINT) LANGUAGE PLPGSQL AS
-  $block2$
-  DECLARE
-    tot_student_count         INT;
-  BEGIN
-    SELECT INTO tot_student_count COALESCE(SUM(student_count), 0) FROM courses WHERE creator_id = creator_id;
-    UPDATE creators SET student_count = tot_student_count WHERE creator_id = creator_id;
-  END;
-  $block2$;
 
   RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: DONE setting up procedures/functions for update of the creator.student_count.';
 END
