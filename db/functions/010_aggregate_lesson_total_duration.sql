dbcdafbb6d69647bfe15f58d7271b4fbf65d80d6
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-11 20:30:56 +0100

    renamed parameter names to avoid conflict, returned row references in triggers, returned the correct row references, scoped column names

--- db/functions/010_aggregate_lesson_total_duration.sql
+++ db/functions/010_aggregate_lesson_total_duration.sql
@@ -9,15 +9,17 @@ BEGIN
   $block1$
   BEGIN
     CALL add_lesson_content_equiv_lesson_dur_and_count(NEW.lesson_id, NEW.duration);
+		RETURN NEW;
   END;
   $block1$ LANGUAGE PLPGSQL;
 
   CREATE OR REPLACE PROCEDURE add_lesson_content_equiv_lesson_dur_and_count
-  (lesson_id UUID, duration INT) LANGUAGE PLPGSQL AS
+  (lesson_id_ UUID, duration INT) LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
     UPDATE lessons SET total_duration = (total_duration + duration),
-    content_count = (content_count + 1) WHERE lesson_id = lesson_id;
+    content_count = (content_count + 1)
+		WHERE lessons.lesson_id = lesson_id_;
   END;
   $block2$;
 
@@ -30,15 +32,17 @@ BEGIN
   $block1$
   BEGIN
     CALL subtract_lesson_content_equiv_lesson_dur_and_count(OLD.lesson_id, OLD.duration);
+		RETURN OLD;
   END;
   $block1$ LANGUAGE PLPGSQL;
 
   CREATE OR REPLACE PROCEDURE subtract_lesson_content_equiv_lesson_dur_and_count
-  (lesson_id UUID, duration INT) LANGUAGE PLPGSQL AS
+  (lesson_id_ UUID, duration INT) LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
     UPDATE lessons SET total_duration = (total_duration - duration),
-    content_count = (content_count - 1) WHERE lesson_id = lesson_id;
+    content_count = (content_count - 1)
+		WHERE lessons.lesson_id = lesson_id_;
   END;
   $block2$;
 
