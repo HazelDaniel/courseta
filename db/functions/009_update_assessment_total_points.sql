af43293b495f2fcc1f5bfab237ff1e7c26552780
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-11 20:30:18 +0100

    renamed parameter names to avoid conflict, returned row references in triggers, returned the correct row references, scoped column names

--- db/functions/009_update_assessment_total_points.sql
+++ db/functions/009_update_assessment_total_points.sql
@@ -9,15 +9,17 @@ BEGIN
   $block1$
   BEGIN
     CALL add_question_equiv_assessment_points_and_count(NEW.assessment_id, NEW.points);
+		RETURN NEW;
   END;
   $block1$ LANGUAGE PLPGSQL;
 
   CREATE OR REPLACE PROCEDURE add_question_equiv_assessment_points_and_count
-  (assessment_id UUID, new_point INT) LANGUAGE PLPGSQL AS
+  (assessment_id_ UUID, new_point INT) LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
     UPDATE assessments SET total_points = (total_points + new_point),
-    question_count = (question_count + 1) WHERE assessment_id = assessment_id;
+    question_count = (question_count + 1)
+		WHERE assessments.assessment_id = assessment_id_;
   END;
   $block2$;
 
@@ -30,15 +32,17 @@ BEGIN
   $block1$
   BEGIN
     CALL subtract_question_equiv_assessment_points_and_count(OLD.assessment_id, OLD.points);
+		RETURN OLD;
   END;
   $block1$ LANGUAGE PLPGSQL;
 
   CREATE OR REPLACE PROCEDURE subtract_question_equiv_assessment_points_and_count
-  (assessment_id UUID, old_point INT) LANGUAGE PLPGSQL AS
+  (assessment_id_ UUID, old_point INT) LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
     UPDATE assessments SET total_points = (total_points + old_point),
-    question_count = (question_count - 1) WHERE assessment_id = assessment_id;
+    question_count = (question_count - 1)
+		WHERE assessments.assessment_id = assessment_id_;
   END;
   $block2$;
 
