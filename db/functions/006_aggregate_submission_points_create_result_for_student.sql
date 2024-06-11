08cf5ee912e4318695b5541999b84f203ece5df0
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-11 20:25:17 +0100

    scoped column names

--- db/functions/006_aggregate_submission_points_create_result_for_student.sql
+++ db/functions/006_aggregate_submission_points_create_result_for_student.sql
@@ -10,20 +10,22 @@ BEGIN
     tot_question_points    INT;
     gen_result_id          UUID;
   BEGIN
-    SELECT INTO retake_points COALESCE(SUM(total_points_accumulated), 0) FROM students__assessments WHERE
-    student_id = NEW.student_id AND assessment_id = NEW.assessment_id
-    AND submitted_at <> NEW.submitted_at;
+    SELECT INTO retake_points COALESCE(SUM(total_points_accumulated), 0) FROM students__assessments
+		WHERE students__assessments.student_id = NEW.student_id
+		AND students__assessments.assessment_id = NEW.assessment_id
+    AND students__assessments.submitted_at <> NEW.submitted_at;
 
     UPDATE students SET points = (points - retake_points)
-    WHERE student_id = NEW.student_id; -- deducting existing retake points from the students points
+    WHERE students.student_id = NEW.student_id; -- deducting existing retake points from the students points
 
     SELECT INTO tot_question_points COALESCE(SUM(points_accumulated), 0) FROM students__questions
     JOIN questions USING (question_id)
-    WHERE assessment_id = NEW.assessment_id AND answered_at = NEW.submitted_at
-    AND student_id = NEW.student_id;
+    WHERE questions.assessment_id = NEW.assessment_id AND questions.answered_at = NEW.submitted_at
+    AND students__questions.student_id = NEW.student_id;
 
     NEW.total_points_accumulated = tot_question_points;
-    UPDATE students SET points = points + tot_question_points WHERE student_id = NEW.student_id;
+    UPDATE students SET points = points + tot_question_points
+		WHERE students.student_id = NEW.student_id;
     -- we should create a corresponding assessment result here
     gen_result_id := insert_equiv_assessment_result(NEW.student_id, NEW.assessment_id, NEW.submitted_at);
     CALL update_score_on_assessment_result(NEW.student_id, NEW.assessment_id, NEW.submitted_at, gen_result_id);
