f8cf3e866bd369546862459a10480b2a33845cb1
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-11 20:22:53 +0100

    renamed parameter names to avoid conflict

--- db/functions/005_create_assesment_result_compute_attribute.sql
+++ db/functions/005_create_assesment_result_compute_attribute.sql
@@ -6,7 +6,7 @@ BEGIN
   RAISE NOTICE '<[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for update on the assessment_results.score.';
 
   CREATE OR REPLACE PROCEDURE update_score_on_assessment_result
-  (student_id UUID, assessment_id UUID, time_attempted TIMESTAMPTZ, result_id UUID)
+  (student_id_ UUID, assessment_id_ UUID, time_attempted TIMESTAMPTZ, result_id UUID)
   LANGUAGE PLPGSQL AS
   $block2$
   DECLARE
@@ -15,16 +15,19 @@ BEGIN
     res             INT;
   BEGIN
     SELECT INTO acc_points total_points_accumulated
-    FROM students__assessments WHERE submitted_at = time_attempted
-    AND student_id = student_id AND assessment_id = assessment_id;
+    FROM students__assessments
+		WHERE students__assessments.submitted_at = time_attempted
+    AND students__assessments.student_id = student_id_
+		AND students__assessments.assessment_id = assessment_id_;
 
     SELECT INTO tot_points total_points
-    FROM assessments WHERE assessment_id = assessment_id;
+    FROM assessments WHERE assessments.assessment_id = assessment_id_;
 
     res := ((acc_points / tot_points) * 100)::INT;
 
-    UPDATE assessment_results SET score = res WHERE attempted_at = time_attempted
-    AND student_id = student_id AND assessment_id = assessment_id;
+    UPDATE assessment_results SET score = res
+		WHERE attempted_at = time_attempted
+    AND assessments_results.student_id = student_id_ AND assessments_results.assessment_id = assessment_id_;
   END;
   $block2$;
 
@@ -33,7 +36,7 @@ BEGIN
   RAISE NOTICE '<[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for creation of assessment_result.';
 
   CREATE OR REPLACE FUNCTION insert_equiv_assessment_result
-  (student_id UUID, assessment_id UUID, time_attempted TIMESTAMPTZ)
+  (student_id_ UUID, assessment_id_ UUID, time_attempted TIMESTAMPTZ)
   RETURNS UUID
   AS
   $block2$
@@ -41,7 +44,7 @@ BEGIN
     res           UUID;
   BEGIN
     INSERT INTO assessment_results (student_id, assessment_id, submitted_at)
-    VALUES (student_id, assessment_id, time_attempted) RETURNING asessment_result_id INTO res;
+    VALUES (student_id_, assessment_id_, time_attempted) RETURNING asessment_result_id INTO res;
 
     RETURN res;
   END;
