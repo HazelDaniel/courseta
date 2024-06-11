512b59793c27663a0231dc1dcf044e898ffb84ee
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-11 20:35:35 +0100

    updated the logic for updating course review count on the creators table, corrected the parameter types, renamed parameter names to avoid conflict, returned row references in triggers, returned the correct row references, scoped column names

--- db/functions/014_aggregate_creator_course_review_count.sql
+++ db/functions/014_aggregate_creator_course_review_count.sql
@@ -9,18 +9,30 @@ BEGIN
     IF NEW.review_count <> OLD.review_count THEN
       CALL update_course_review_equiv_creator_count(NEW.creator_id);
     END IF;
+
+		IF TG_OP = 'DELETE' THEN
+			RETURN OLD;
+		END IF;
+
     RETURN NEW;
   END;
   $block1$ LANGUAGE PLPGSQL;
 
   CREATE OR REPLACE PROCEDURE update_course_review_equiv_creator_count
-  (creator_id BIGINT) LANGUAGE PLPGSQL AS
+  (creator_id_ UUID) LANGUAGE PLPGSQL AS
   $block2$
   DECLARE
     tot_review_count         INT;
   BEGIN
-    SELECT INTO tot_review_count COALESCE(SUM(review_count), 0) FROM courses WHERE creator_id = creator_id;
-    UPDATE creators SET course_review_count = tot_review_count WHERE creator_id = creator_id;
+    SELECT INTO tot_review_count review_count_ FROM(
+    SELECT COALESCE(SUM(review_count), 0) review_count_
+    FROM courseta.courses
+    WHERE courses.creator_id = creator_id_
+		GROUP BY creator_id) as RES;
+
+    UPDATE courseta.creators
+    SET course_review_count = tot_review_count
+    WHERE creators.creator_id = creator_id_;
   END;
   $block2$;
 
