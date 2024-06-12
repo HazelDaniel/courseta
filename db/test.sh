14cbd320f668972a2ccd20bd7aaf51e6baa663c5
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-12 21:16:57 +0100

    added ability to add 'test skip' directives, added colored output for better readability

--- db/test.sh
+++ db/test.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 # this runs the test suite for the DB
 source ./.environment.zshrc
+x=1
 for i in $(find ./tests -type f | sort -n); do
+	if head -n 1 "$i" | grep -Piq "__test-ignore__" "$i";
+	then
+		((x += 1));
+		continue;
+	fi
+	echo -e "[\e[32mUNIT\e[0m]: RUNNING TEST${x} <=> ${i}...";
 	./setup.sh && psql -Utoughware -p5432 -hlocalhost -d courseta -f "$i";
+	echo -e "[\e[32mUNIT\e[0m]: DONE RUNNING TEST${x} <=> ${i}. (STATUS: " $?")";
+	((x += 1));
+	echo "";
+	echo "";
 done
