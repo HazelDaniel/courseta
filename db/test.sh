b9426771f233c2f88358a8a7e085bcdd7992f032
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-13 13:00:44 +0100

    added ability to skip test fixtures for empty files

--- db/test.sh
+++ db/test.sh
@@ -3,15 +3,19 @@
 source ./.environment.zshrc
 x=1
 for i in $(find ./tests -type f | sort -n); do
-	if head -n 1 "$i" | grep -Piq "__test-ignore__" "$i";
+	y="$(wc -l "$i" | cut -d ' ' -f 1)"
+
+	if head -n 1 "$i" | grep -Piq "__test-ignore__" "$i" || [[ $y -lt 1 ]];
 	then
 		((x += 1));
 		continue;
 	fi
+
 	echo -e "[\e[32mUNIT\e[0m]: RUNNING TEST${x} <=> ${i}...";
 	./setup.sh && psql -Utoughware -p5432 -hlocalhost -d courseta -f "$i";
 	echo -e "[\e[32mUNIT\e[0m]: DONE RUNNING TEST${x} <=> ${i}. (STATUS: " $?")";
 	((x += 1));
+
 	echo "";
 	echo "";
 done
