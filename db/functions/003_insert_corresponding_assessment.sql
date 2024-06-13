d6c263bf2b2acaba32df412c403043127a20f622
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-13 16:45:01 +0100

    added the description field as a parameter for procecures that creates a corresponding assessment id, overrode the assessment type for exams (i guess)

--- db/functions/003_insert_corresponding_assessment.sql
+++ db/functions/003_insert_corresponding_assessment.sql
@@ -5,16 +5,16 @@ BEGIN
   CREATE OR REPLACE FUNCTION insert_exam_equiv_assessment () RETURNS TRIGGER AS
   $block1$
     BEGIN
-      CALL insert_assessment_with_exam_id(NEW.exam_id);
+      CALL insert_assessment_with_exam_id(NEW.exam_id, NEW.description, 'exam');
       RETURN NEW;
     END;
   $block1$ LANGUAGE PLPGSQL;
 
-  CREATE OR REPLACE PROCEDURE insert_assessment_with_exam_id (exam_id UUID, assessment_type courseta.ASSESSMENT_TYPE)
+  CREATE OR REPLACE PROCEDURE insert_assessment_with_exam_id (exam_id UUID, exam_description TEXT, assessment_type courseta.ASSESSMENT_TYPE)
   LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
-    INSERT INTO assessments (assessment_id, assessment_type) VALUES (exam_id, assessment_type);
+    INSERT INTO assessments (assessment_id, description, assessment_type) VALUES (exam_id, exam_description, assessment_type);
   END;
   $block2$;
 
@@ -22,16 +22,16 @@ BEGIN
   CREATE OR REPLACE FUNCTION insert_quiz_equiv_assessment () RETURNS TRIGGER AS
   $block3$
     BEGIN
-      CALL insert_assessment_with_quiz_id(NEW.quiz_id);
+      CALL insert_assessment_with_quiz_id(NEW.quiz_id, NEW.description);
       RETURN NEW;
     END;
   $block3$ LANGUAGE PLPGSQL;
 
-  CREATE OR REPLACE PROCEDURE insert_assessment_with_quiz_id (quiz_id UUID)
+  CREATE OR REPLACE PROCEDURE insert_assessment_with_quiz_id (quiz_id UUID, quiz_description TEXT)
   LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
-    INSERT INTO assessments (assessment_id) VALUES (quiz_id);
+    INSERT INTO assessments (assessment_id, description) VALUES (quiz_id, quiz_description);
   END;
   $block2$;
   RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: DONE setting up procedures/functions for assessment insertion.';
