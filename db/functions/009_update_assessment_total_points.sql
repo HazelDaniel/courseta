c759cf07e48dff6f3485acc8e02341bd07db56a7
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-13 16:47:16 +0100

    added another procedure/function to update only the total points for an assessment on 'update' operation, improved the correctness of the algorithm that reduces the existing points of an assessment

--- db/functions/009_update_assessment_total_points.sql
+++ db/functions/009_update_assessment_total_points.sql
@@ -3,7 +3,35 @@ $block$
 BEGIN
   RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for update of the assessment.total_points.';
 
+  CREATE OR REPLACE PROCEDURE update_question_equiv_assessment_points
+  (assessment_id_ UUID, new_point INT) LANGUAGE PLPGSQL AS
+  $block2$
+  BEGIN
+    UPDATE assessments SET total_points = (total_points + new_point)
+		WHERE assessments.assessment_id = assessment_id_;
+  END;
+  $block2$;
+
+  CREATE OR REPLACE FUNCTION update_points_and_question_count_on_assessment () RETURNS TRIGGER AS
+  $block1$
+  BEGIN
+    CALL update_question_equiv_assessment_points(NEW.assessment_id, NEW.points - OLD.points);
+		RETURN NEW;
+  END;
+  $block1$ LANGUAGE PLPGSQL;
+
   RAISE NOTICE '<[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for addition to the assessment.total_points.';
+
+  CREATE OR REPLACE PROCEDURE add_question_equiv_assessment_points_and_count
+  (assessment_id_ UUID, new_point INT) LANGUAGE PLPGSQL AS
+  $block2$
+  BEGIN
+    UPDATE assessments SET total_points = (total_points + new_point),
+    question_count = (question_count + 1)
+		WHERE assessments.assessment_id = assessment_id_;
+  END;
+  $block2$;
+
 
   CREATE OR REPLACE FUNCTION add_points_and_question_count_to_assessment () RETURNS TRIGGER AS
   $block1$
@@ -40,7 +68,7 @@ BEGIN
   (assessment_id_ UUID, old_point INT) LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
-    UPDATE assessments SET total_points = (total_points + old_point),
+    UPDATE assessments SET total_points = (total_points - old_point),
     question_count = (question_count - 1)
 		WHERE assessments.assessment_id = assessment_id_;
   END;
