5eedf28ae527a7f4c4a7c40d32b5e0e35b84f882
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-15 22:11:35 +0100

    made sure that the data format returned in the result set conforms with what is needed in the API

--- db/GET-functions/get_student_quiz_results.sql
+++ db/GET-functions/get_student_quiz_results.sql
@@ -7,14 +7,17 @@ BEGIN
   TABLE (
     quiz_id UUID,
     date_completed TIMESTAMPTZ,
+    course_id BIGINT,
     score SMALLINT
   ) AS
   $block1$
   BEGIN
     RETURN QUERY SELECT
-    quizzes.quiz_id, assessments_results.attempted_at date_completed, assessments_results.score
+    quizzes.quiz_id, assessments_results.attempted_at date_completed, courses.course_id, assessments_results.score
     FROM assessments_results
     JOIN quizzes USING (assessment_id)
+    JOIN lessons USING (lesson_id)
+    JOIN courses USING (course_id)
     WHERE assessments_results.student_id = student_id_;
   END;
   $block1$ LANGUAGE PLPGSQL;
