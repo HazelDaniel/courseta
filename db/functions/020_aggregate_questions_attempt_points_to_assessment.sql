6c1ab339cfe2053ae263025a09e42349f0a2b9dd
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-15 09:38:28 +0100

    wrote functions to aggregate assessment points from question attempts

---
+++ db/functions/020_aggregate_questions_attempt_points_to_assessment.sql
@@ -0,0 +1,49 @@
+DO
+$block$
+BEGIN
+  RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for update on the students__questions.points_accumulated column.';
+
+  CREATE OR REPLACE FUNCTION agg_questions_attempt_points_to_submission () RETURNS TRIGGER AS
+  $block1$
+  DECLARE
+    equiv_assessment_id             UUID;
+    last_answers_agg_points          INT;
+    debug_answer_record                RECORD;
+  BEGIN
+    IF NEW.points_accumulated = OLD.points_accumulated THEN
+      RETURN NEW; -- don't do anything further if the update is not on the points_accumulated field
+    END IF;
+
+
+    SELECT INTO equiv_assessment_id assessments.assessment_id
+    FROM students__questions
+    JOIN questions USING (question_id)
+    JOIN assessments USING (assessment_id)
+    WHERE students__questions.question_id = NEW.question_id;
+
+
+    WITH last_answers_agg_list AS (
+    SELECT ROW_NUMBER() OVER (PARTITION BY question_id ORDER BY updated_at DESC), *
+    FROM students__answers
+    WHERE students__answers.student_id = NEW.student_id
+    AND students__answers.selected_at = NEW.answered_at)
+
+    SELECT INTO last_answers_agg_points COALESCE(SUM(points_gained), 0)
+    FROM last_answers_agg_list
+    WHERE row_number = 1;
+
+    UPDATE courseta.students__assessments
+    SET total_points_accumulated = last_answers_agg_points
+		WHERE students__assessments.student_id = NEW.student_id
+    AND students__assessments.submitted_at = NEW.answered_at
+    AND students__assessments.assessment_id = equiv_assessment_id;
+
+    RETURN NEW;
+  END;
+  $block1$ LANGUAGE PLPGSQL;
+
+  RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: DONE setting up procedures/functions for update on the students__questions.points_accumulated column.';
+
+END
+$block$ LANGUAGE PLPGSQL;
+
