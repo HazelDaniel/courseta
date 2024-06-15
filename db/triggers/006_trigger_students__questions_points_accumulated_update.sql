fc191298159390dcc8851d3421fe4c7c47da4d46
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-15 09:33:59 +0100

    updated the trigger for answer submission points to question to fire after insert on the students__answers table

--- db/triggers/006_trigger_students__questions_points_accumulated_update.sql
+++ db/triggers/006_trigger_students__questions_points_accumulated_update.sql
@@ -4,7 +4,7 @@ BEGIN
   RAISE NOTICE '[SETUP]   TRIGGER: updating students__questions.points_accumulated update triggers...';
   CREATE OR REPLACE TRIGGER trigger_students__questions_points_accumulated_update
   AFTER INSERT
-  ON students__questions
+  ON students__answers
   FOR EACH ROW
   EXECUTE FUNCTION agg_answer_submission_points_to_question();
 
