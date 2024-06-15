8901a2fa7d29a00e41d3ff1793a041b6f3064a2c
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-15 09:24:55 +0100

    made the update_equiv_course_progress only run once for true submissions to complete the unit of work from the question attempt phase to the  point aggregation phase, made the calculation of the progress more precise , introduced more variables for easy calculations

--- db/functions/017_update_student__course_progress_count.sql
+++ db/functions/017_update_student__course_progress_count.sql
@@ -6,39 +6,56 @@ BEGIN
   CREATE OR REPLACE FUNCTION update_equiv_course_progress () RETURNS TRIGGER AS
   $block1$
     BEGIN
+      IF NEW.waiting = OLD.waiting THEN
+        RETURN NEW; -- dont do anything further if this is not a true assessment submission
+      END IF;
+
       CALL update_progress_for_course(NEW.assessment_id, NEW.student_id, NEW.submitted_at);
 
       RETURN NEW;
     END;
   $block1$ LANGUAGE PLPGSQL;
 
-  CREATE OR REPLACE PROCEDURE update_progress_for_course (assessment_id UUID, student_id UUID, submitted_at TIMESTAMPTZ)
+  CREATE OR REPLACE PROCEDURE update_progress_for_course (assessment_id_ UUID, student_id_ UUID, submitted_at_ TIMESTAMPTZ)
   LANGUAGE PLPGSQL AS
   $block2$
   DECLARE
     course_entry       RECORD;
-    attempt_count      INT;
+    attempt_count         INT;
+    quiz_count_           INT;
+    precise_progress            NUMERIC;
   BEGIN
-    SELECT INTO course_entry course_id, quiz_count FROM (SELECT students__assessments.assessment_id, courses.course_id
-    FROM courseta.students__assessments
-    JOIN courseta.quizzes ON (students__assessments.assessment_id = quizzes.quiz_id)
-    JOIN courseta.lessons USING (lesson_id)
-    JOIN courseta.courses USING (course_id)
-    WHERE courseta.students__assessments.assessment_id = assessment_id
-    AND courseta.students__assessments.submitted_at = submitted_at) AS RES;
 
-    SELECT INTO attempt_count COUNT(*) FROM(
-      SELECT students__assessments.student_id, students__assessments.assesssment_id, lessons.lesson_id, courses.course_id
+    SELECT courses.course_id
       FROM courseta.students__assessments
       JOIN courseta.quizzes ON (students__assessments.assessment_id = quizzes.quiz_id)
       JOIN courseta.lessons USING (lesson_id)
       JOIN courseta.courses USING (course_id)
-      WHERE courses.course_id = course_entry.course_id
-      AND students__assessments.student_id = student_id
-      GROUP BY assessment_id
-    ) AS RES2 WHERE RES2.course_id = course_entry.course_id;
+      WHERE courseta.students__assessments.assessment_id = assessment_id_
+      AND courseta.students__assessments.submitted_at = submitted_at_
+    INTO course_entry;
 
-    UPDATE courseta.students__courses SET progress = progress + (attempt_count / COALESCE(NULLIF (quiz_count, 0), attempt_count)) * 100 WHERE students__courses.course_id = course_entry.course_id;
+    SELECT INTO attempt_count COUNT(*) FROM(
+      SELECT students__assessments.assessment_id
+      FROM courseta.quizzes
+      JOIN courseta.students__assessments ON (students__assessments.assessment_id = quizzes.quiz_id)
+      JOIN courseta.lessons USING (lesson_id)
+      JOIN courseta.courses USING (course_id)
+      WHERE courses.course_id = course_entry.course_id
+      AND students__assessments.student_id = student_id_
+      GROUP BY students__assessments.assessment_id
+    ) AS RES2;
+
+    SELECT INTO quiz_count_ courses.quiz_count
+    FROM courseta.courses WHERE
+    course_id = course_entry.course_id;
+
+    precise_progress := (attempt_count::NUMERIC / COALESCE(NULLIF(quiz_count_, 0), attempt_count)) * 100;
+
+
+    UPDATE courseta.students__courses SET progress = progress + precise_progress
+    WHERE students__courses.course_id = course_entry.course_id;
+
 
   END;
   $block2$;
