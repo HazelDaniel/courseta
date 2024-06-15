1eaaa90864c5eead73de7c2c3c557f50ba011f02
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-15 09:32:41 +0100

    updated the trigger for submission points aggregation on student to fire after update

--- db/triggers/005_trigger_user_points_update_create_assessment_result.sql
+++ db/triggers/005_trigger_user_points_update_create_assessment_result.sql
@@ -2,8 +2,8 @@ DO
 $block$
 BEGIN
   RAISE NOTICE '[SETUP]   TRIGGER: updating students.points update triggers...';
-  CREATE OR REPLACE TRIGGER trigger_student_points_update
-  AFTER INSERT
+  CREATE OR REPLACE TRIGGER trigger_01_student_points_update
+  AFTER UPDATE
   ON students__assessments
   FOR EACH ROW
   EXECUTE FUNCTION agg_assessment_submission_points_to_student();
