67586923f3922e5b020d52d8bd5a67998f14f1c7
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-16 14:23:55 +0100

    improved the correctness of the recommendation algorithm, removed unnecessary joins

--- db/GET-functions/get_recommended_courses_for_student.sql
+++ db/GET-functions/get_recommended_courses_for_student.sql
@@ -22,16 +22,15 @@ BEGIN
     DISTINCT ON (courses.course_id) courses.title, courses.lesson_count, courses.thumbnail, courses.course_id, students__courses.enrolled_at created_at
     FROM courseta.students__courses
     JOIN courseta.courses USING (course_id)
-    JOIN courseta.reviews USING (course_id)
     WHERE students__courses.student_id <> student_id_
-    AND reviews.rating >= '4'
+    AND courses.average_rating >= 4.0
+    -- AND courses.student_count >= 1 (NO NEED TO CHECK THIS, THERE WON'T BE a corresponding students__courses if the student count is 0 so, empty set)
     AND courses.tags && enrolled_courses_tags),
     student_enrolled_courses AS
     (SELECT
     DISTINCT ON (courses.course_id) courses.course_id
     FROM courseta.students__courses
     JOIN courseta.courses USING (course_id)
-    JOIN courseta.reviews USING (course_id)
     WHERE students__courses.student_id = student_id_
     )
     SELECT
@@ -39,7 +38,7 @@ BEGIN
     FROM recommended_courses rc
     LEFT JOIN student_enrolled_courses sec USING (course_id)
     WHERE sec.course_id IS NULL
-    ORDER BY rc.course_id, rc.created_at AT TIME ZONE('UTC') DESC
+    ORDER BY rc.created_at AT TIME ZONE('UTC') DESC, rc.course_id
     LIMIT 10);
   END;
   $block1$ LANGUAGE PLPGSQL;
