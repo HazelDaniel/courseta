cb0ffda1ef394a5762c07cddb125e52a852c7788
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-17 00:36:58 +0100

    added avatar_url_field as a returned attribute in the result set for students too

--- db/GET-functions/get_current_student.sql
+++ db/GET-functions/get_current_student.sql
@@ -4,11 +4,19 @@ BEGIN
   RAISE NOTICE '[SETUP]   (GET) FUNCTION: setting up the get_current_student function ...';
 
   CREATE OR REPLACE FUNCTION get_current_student (email_ TEXT) RETURNS
-  TABLE (student_id UUID, rank courseta.RANK_TYPE, points INT, email VARCHAR, role courseta.USER_ROLE_TYPE)
+  TABLE
+  (
+   student_id UUID,
+   rank courseta.RANK_TYPE,
+   points INT,
+   email VARCHAR,
+   role courseta.USER_ROLE_TYPE,
+   avatar_url TEXT
+  )
   AS
   $block1$
   BEGIN
-    RETURN QUERY SELECT students.student_id, students.rank, students.points, students.email, students.role
+    RETURN QUERY SELECT students.student_id, students.rank, students.points, students.email, students.role, students.avatar_url
     FROM courseta.students
     WHERE students.email = email_;
   END;
