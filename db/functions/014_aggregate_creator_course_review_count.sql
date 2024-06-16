8593c076c8afae319f8c94fe2376e95e2fa929b7
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-16 14:27:45 +0100

    improved the query

--- db/functions/014_aggregate_creator_course_review_count.sql
+++ db/functions/014_aggregate_creator_course_review_count.sql
@@ -22,16 +22,17 @@ BEGIN
   (creator_id_ UUID) LANGUAGE PLPGSQL AS
   $block2$
   DECLARE
-    tot_review_count         INT;
+    tot_review         RECORD;
   BEGIN
-    SELECT INTO tot_review_count review_count_ FROM(
-    SELECT COALESCE(SUM(review_count), 0) review_count_
+    SELECT INTO tot_review review_count_, avg_rating FROM(
+    SELECT COALESCE(SUM(review_count), 0) review_count_, AVG(average_rating) avg_rating
     FROM courseta.courses
     WHERE courses.creator_id = creator_id_
 		GROUP BY creator_id) as RES;
 
     UPDATE courseta.creators
-    SET course_review_count = tot_review_count
+    SET course_review_count = tot_review.review_count_,
+    average_course_rating = tot_review.avg_rating::NUMERIC(2, 1)
     WHERE creators.creator_id = creator_id_;
   END;
   $block2$;
