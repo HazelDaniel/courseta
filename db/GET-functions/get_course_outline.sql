d86aaf20b0d6ac1b4e00802f4b089a0d60be7b46
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-17 23:05:05 +0100

    added a GET function to retrieve the lessons and quizzes of a course returning the result set used in the API

---
+++ db/GET-functions/get_course_outline.sql
@@ -0,0 +1,30 @@
+DO
+$block$
+BEGIN
+  RAISE NOTICE '[SETUP]   (GET) FUNCTION: setting up the get_course_outline function ...';
+
+  CREATE OR REPLACE FUNCTION get_course_outline (course_id_ BIGINT) RETURNS
+  TABLE (
+    lesson_id BIGINT,
+    title VARCHAR,
+    course_id BIGINT,
+    content_count SMALLINT,
+    total_duration SMALLINT,
+    quiz_id UUID,
+    total_points INT,
+    quiz_title TEXT
+  ) AS
+  $block1$
+  BEGIN
+    RETURN QUERY SELECT lessons.lesson_id, lessons.title, lessons.course_id,
+    lessons.content_count, lessons.total_duration, quizzes.quiz_id, quizzes.total_points, quizzes.quiz_title
+    FROM courseta.lessons
+    LEFT JOIN courseta.quizzes USING (lesson_id)
+    WHERE lessons.course_id = course_id_;
+  END;
+  $block1$ LANGUAGE PLPGSQL;
+
+  RAISE NOTICE '[SETUP]   (GET) FUNCTION: DONE setting up the get_course_outline function.';
+END
+$block$ LANGUAGE PLPGSQL;
+
