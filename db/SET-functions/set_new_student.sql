cd1e8be17b28bec6a7b485c7a81c9c5bad1586fd
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-19 09:55:56 +0100

    added a function overload and added an avatar url parameter so as to receive the avatar url from the API

--- db/SET-functions/set_new_student.sql
+++ db/SET-functions/set_new_student.sql
@@ -4,20 +4,47 @@ BEGIN
   RAISE NOTICE '[SETUP]   (SET) FUNCTION: setting up the set_new_student function ...';
 
   CREATE OR REPLACE FUNCTION set_new_student (
+  -- +1 OVERLOADS
     email_ VARCHAR,
     first_name_ VARCHAR,
     last_name_ VARCHAR,
-    password_ TEXT
+    password_ TEXT,
+    avatar_url TEXT
   )
-  RETURNS TABLE (student_id UUID, rank courseta.RANK_TYPE, points INT, email VARCHAR) AS
+  RETURNS TABLE (student_id UUID, rank courseta.RANK_TYPE, points INT, email VARCHAR, avatar JSONB) AS
   $block1$
   BEGIN
     RETURN QUERY 
-    INSERT INTO students (email, first_name, last_name, password)
-    VALUES (email_, first_name_, last_name_, password_)
-    RETURNING students.student_id, students.rank, students.points, students.email;
+    INSERT INTO students (email, first_name, last_name, password, avatar, created_at)
+    VALUES (email_, first_name_, last_name_, password_,
+    ('{"url": "' || avatar_url || '", "created_at": "' ||  CURRENT_TIMESTAMP::TEXT || '", "updated_at" : "' || CURRENT_TIMESTAMP::TEXT || '"}')::JSONB,
+    CURRENT_TIMESTAMP)
+    RETURNING students.student_id, students.rank, students.points, students.email, students.avatar;
   END;
   $block1$ LANGUAGE PLPGSQL;
+
+
+  CREATE OR REPLACE FUNCTION set_new_student (
+  -- [OVERLOAD]: for testing
+    email_ VARCHAR,
+    first_name_ VARCHAR,
+    last_name_ VARCHAR,
+    password_ TEXT,
+    avatar_url TEXT,
+    student_id_ UUID
+  )
+  RETURNS TABLE (student_id UUID, rank courseta.RANK_TYPE, points INT, email VARCHAR, avatar JSONB) AS
+  $block1$
+  BEGIN
+    RETURN QUERY
+    INSERT INTO students (email, first_name, last_name, password, avatar, created_at, student_id)
+    VALUES (email_, first_name_, last_name_, password_,
+    ('{"url": "' || avatar_url || '", "created_at": "' ||  CURRENT_TIMESTAMP::TEXT || '", "updated_at" : "' || CURRENT_TIMESTAMP::TEXT || '"}')::JSONB,
+    CURRENT_TIMESTAMP, student_id_)
+    RETURNING students.student_id, students.rank, students.points, students.email, students.avatar;
+  END;
+  $block1$ LANGUAGE PLPGSQL;
+
 
   RAISE NOTICE '[SETUP]   (SET) FUNCTION: DONE setting up the set_new_student function.';
 END;
