5e4206e173624f6dac848441c9a3eb2b787bb213
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-19 09:57:57 +0100

    updated the test suite

--- db/tests/001_test_student_recommended_course_feature.sql
+++ db/tests/001_test_student_recommended_course_feature.sql
@@ -1,4 +1,3 @@
--- __test-ignore__
 \x
 \set courseta_var_creator_id_1 2aac9187-e2e9-4326-9c0b-920bfa0143b4
 \set courseta_var_creator_id_2 47621eb8-08e0-42d4-87cb-8efac5676290
@@ -7,20 +6,12 @@
 \set courseta_var_student_id_2 a69c1374-d4ef-4644-a791-fd8b6601aa8c
 \set courseta_var_student_id_3 a69c1304-d4af-4644-c082-fd8b6601aa8d
 
-INSERT INTO creators (email, first_name, last_name, password, creator_id)
-VALUES ('hazel856@gmail.com', 'hazel', 'daniel', '8080', :'courseta_var_creator_id_1') ON CONFLICT DO NOTHING;
+SELECT set_new_creator('hazel856@gmail.com', 'hazel', 'daniel', '8080', '', :'courseta_var_creator_id_1');
+SELECT set_new_creator('creator2hazel856@gmail.com', '2hazel', '2daniel', '28080', '', :'courseta_var_creator_id_2');
 
-INSERT INTO creators (email, first_name, last_name, password, creator_id)
-VALUES ('creator2hazel856@gmail.com', '2hazel', '2daniel', '28080', :'courseta_var_creator_id_2') ON CONFLICT DO NOTHING;
-
-INSERT INTO students (email, first_name, last_name, password, student_id)
-VALUES ('studenthazel@gmail.com', 'studenthazel1', 'daniel', '28080', :'courseta_var_student_id_1') ON CONFLICT DO NOTHING;
-
-INSERT INTO students (email, first_name, last_name, password, student_id)
-VALUES ('studenthazel2@gmail.com', 'studenthazel2', 'daniel2', '280fs80', :'courseta_var_student_id_2') ON CONFLICT DO NOTHING;
-
-INSERT INTO students (email, first_name, last_name, password, student_id)
-VALUES ('studenthazel3@gmail.com', 'studenthazel3', 'daniel3', '2803s80', :'courseta_var_student_id_3') ON CONFLICT DO NOTHING;
+SELECT set_new_student('studenthazel@gmail.com', 'studenthazel1', 'daniel', '28080', '', :'courseta_var_student_id_1');
+SELECT set_new_student('studenthazel2@gmail.com', 'studenthazel2', 'daniel2', '280fs80', '', :'courseta_var_student_id_2');
+SELECT set_new_student('studenthazel3@gmail.com', 'studenthazel3', 'daniel3', '2803s80', '', :'courseta_var_student_id_3');
 
 TRUNCATE TABLE courseta.courses CASCADE;
 
