a9598949824a411e1b44af7b6df35a4dee588c17
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-21 18:57:00 +0100

    added a helper function to validate a creator based on email and password match, returned the right values from the query

--- db/GET-functions/get_current_creator.sql
+++ db/GET-functions/get_current_creator.sql
@@ -7,7 +7,17 @@ BEGIN
   TABLE (creator_id UUID, email VARCHAR, role USER_ROLE_TYPE, creator_pass TEXT, avatar_url TEXT) AS
   $block1$
   BEGIN
-    RETURN QUERY SELECT creators.creator_id, creators.email, creators.role, creators.creator_pass, creators.avatar_url
+    RETURN QUERY SELECT creators.creator_id, creators.email, creators.role, creators.creator_pass, creators.avatar->>'url' avatar_url
+    FROM creators
+    WHERE creators.email = email_;
+  END;
+  $block1$ LANGUAGE PLPGSQL;
+
+  CREATE OR REPLACE FUNCTION get_current_creator_validate (email_ TEXT) RETURNS
+  TABLE (creator_id UUID, creator_pass TEXT, password TEXT) AS
+  $block1$
+  BEGIN
+    RETURN QUERY SELECT creators.creator_id, creators.creator_pass, creators.password
     FROM creators
     WHERE creators.email = email_;
   END;
