0a7899a2ce671fd80b64d73e6c567b6d6708d479
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-22 22:17:08 +0100

    added a function to retrieve a list of courses by a creator

---
+++ db/GET-functions/get_course_summaries_for_creator.sql
@@ -0,0 +1,25 @@
+DO
+$block$
+BEGIN
+  RAISE NOTICE '[SETUP]   (GET) FUNCTION: setting up the get_course_summaries_for_creator function ...';
+
+  CREATE OR REPLACE FUNCTION get_course_summaries_for_creator (creator_id_ UUID) RETURNS
+  TABLE (
+    title TEXT,
+    lesson_count SMALLINT,
+    thumbnail TEXT,
+    course_id BIGINT,
+    average_rating NUMERIC,
+    student_count INT
+  ) AS
+  $block1$
+  BEGIN
+    RETURN QUERY SELECT courses.title, courses.lesson_count, courses.thumbnail, courses.course_id, courses.average_rating, courses.student_count
+    FROM courseta.courses WHERE courses.creator_id = creator_id_;
+  END;
+  $block1$ LANGUAGE PLPGSQL;
+
+  RAISE NOTICE '[SETUP]   (GET) FUNCTION: DONE setting up the get_course_summaries_for_creator function.';
+END
+$block$ LANGUAGE PLPGSQL;
+
