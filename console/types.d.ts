7d7584efd06ed40dce392daf727859aba6d2a2bb
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-22 06:44:51 +0100

    added more types

--- console/types.d.ts
+++ console/types.d.ts
@@ -1,7 +1,7 @@
+import type { PoolClient } from "pg";
 export interface DBSessionFunctionType {
   (client: PoolClient): void;
 }
-
 
 export type UserRoleType = "student" | "creator";
 export type StudentRankType =
@@ -14,12 +14,75 @@ export type StudentRankType =
 
 export type AssessmentVariantType = "exam" | "quiz";
 
-export enum AuthPosition {ADMIN_AUTH, USER_AUTH};
-
 export interface StudentViewType {
-  id: string;
+  studentID: string;
   rank: StudentRankType;
   points: number;
   role: UserRoleType;
   avatarUrl: string;
+}
+
+export interface CreatorViewType {
+  creatorID: string;
+  email: string;
+  role: UserRoleType;
+  creatorPass: string;
+  avatarUrl: string;
+}
+
+export interface AdminViewType {
+  adminID: string;
+  email: string;
+  password: string;
+}
+
+export interface CourseViewType {
+  title: string;
+  lessonCount: number;
+  thumbnail: string;
+  courseID: string;
+}
+
+export interface CourseDetailViewType extends CourseViewType {
+  description: string;
+  reviewCount: number;
+  creatorID: string;
+  studentCount: number;
+  courseLength: number;
+  updatedAt: string;
+}
+
+export interface CourseOutlineViewType {
+  detail: CourseDetailViewType;
+  outline: CourseOutlineType;
+}
+
+export type CourseOutlineType = {
+  lessonID: string;
+  title: string;
+  courseID: string;
+  contentCount: number;
+  totalDuration: number;
+  quizID: string;
+  totalPoints: number;
+  quizTitle: string;
+}[];
+export interface AuthStateType {
+  subject: string | null;
+  adminSubject: string | null;
+  status: [boolean, boolean, boolean];
+}
+
+export type HandlerAccessControlType =
+  | "require-admin"
+  | "require-creator"
+  | "require-student"
+  | "none";
+
+export interface HandlerFunctionType {
+  (authState: AuthStateType, AC: HandlerAccessControlType): void;
+}
+
+export interface UserContractType {
+  verify(email: string | null): Promise<object | null>;
 }
