3f911ad489f4e9fa968ecdc48ec098da2d0f7df5
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-22 14:56:23 +0100

    added utility functions/classes

--- console/utils.ts
+++ console/utils.ts
@@ -9,9 +9,22 @@ export class ConsoleLogger {
       console.log(chalk.redBright(`(FAIL): ${this.message}`));
     else if (this.status === "success")
       console.log(chalk.greenBright(`(SUCCESS): ${this.message}`));
-    else if (this.status === "info") console.log(chalk.white(`(INFO): ${this.message}`));
+    else if (this.status === "info")
+      console.log(chalk.white(`(INFO): ${this.message}`));
     else if (this.status === "error")
       console.log(chalk.bgRedBright(`(ERR): ${this.message}`));
   }
+}
 
+export class BoardDisplay {
+  static level1Nest = "\t\t";
+  static level2Nest = "\t\t\t\t";
+  static level3Nest = "\t\t\t\t\t\t\t\t";
+  static level4Nest = "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t";
+  static level5Nest =
+    "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t";
+  static frameChar = "<>";
+
+  static border = "|";
+  static marginDecoratorCount = 35;
 }
