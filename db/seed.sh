9a3141a69ffbe2342f60504327c27762476f1e74
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-25 14:38:12 +0100

    disabled all triggers before seeding the db to preserve data integrity and enabled them after seeding, fixed typo

--- db/seed.sh
+++ db/seed.sh
@@ -2,18 +2,31 @@
 # populating the db with the stored csv/sql files
 source ./.environment.zshrc
 echo "populating schema..."
-pg_dump -Utoughware -hlocalhost -dcourseta -f "$(find ./backup/schema -type f | sort -n | tail -n 1)" &&\
+psql -Utoughware -hlocalhost -dcourseta -f "$(find ./backup/schema -type f | sort -n | tail -n 1)" &&\
 echo "done" &&\
 
+echo "disabling triggers...";
+for i in `cat ./backup/dependency.txt`; do
+	echo "disabling trigggers before copying: $i..."
+	psql -Utoughware -p5432 -hlocalhost -d courseta -c "ALTER TABLE $i DISABLE TRIGGER ALL" ;
+done &&\
 
 echo "seeding database ..."
 for i in `cat ./backup/dependency.txt`; do
-	# echo $i
 	if [[ ! -d "./backup/tables/$i"  ]]; then
 		mkdir ./backup/tables/"$i"
 	fi
 	dest=$(find "./backup/tables/$i" -type f | sort -n | tail -n 1)
+	echo "using: $dest as destination";
 	psql -Utoughware -p5432 -hlocalhost -d courseta -c "\COPY $i FROM $dest DELIMITER ',' HEADER CSV";
-done
+done &&\
 
-echo "database seeded successfully!"
+echo "enabling triggers...";
+for i in `cat ./backup/dependency.txt`; do
+	echo "enabling trigggers after copying: $i..."
+	psql -Utoughware -p5432 -hlocalhost -d courseta -c "ALTER TABLE $i ENABLE TRIGGER ALL" ;
+done &&\
+echo "updating sequences..." &&\
+psql -Utoughware -p5432 -hlocalhost -d courseta -f ./backup/fix-sequence.sql &&\
+echo "sequence updated!";
+echo "database seeded successfully!";
