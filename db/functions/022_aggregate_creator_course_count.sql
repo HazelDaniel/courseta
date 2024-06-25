8836e3e670083ffe5876d31879298191fdc59d15
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-25 14:32:40 +0100

    wrote a function/procedure that aggregates the course_count attribute on creators table

---
+++ db/functions/022_aggregate_creator_course_count.sql
@@ -0,0 +1,43 @@
+DO
+$block$
+BEGIN
+  RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for update of the creator.course_count.';
+
+  CREATE OR REPLACE PROCEDURE add_course_equiv_creator_count
+  (creator_id_ UUID) LANGUAGE PLPGSQL AS
+  $block2$
+  BEGIN
+    UPDATE courseta.creators
+    SET course_count = course_count + 1
+    WHERE creators.creator_id = creator_id_;
+  END;
+  $block2$;
+
+  CREATE OR REPLACE PROCEDURE subtract_course_equiv_creator_count
+  (creator_id_ UUID) LANGUAGE PLPGSQL AS
+  $block2$
+  BEGIN
+    UPDATE courseta.creators
+    SET course_count = course_count - 1
+    WHERE creators.creator_id = creator_id_;
+  END;
+  $block2$;
+
+  CREATE OR REPLACE FUNCTION update_course_count_on_creator () RETURNS TRIGGER AS
+  $block1$
+  BEGIN
+    CASE TG_OP
+      WHEN 'INSERT' THEN
+        CALL add_course_equiv_creator_count(NEW.creator_id);
+        RETURN NEW;
+      WHEN 'DELETE' THEN
+        CALL subtract_course_equiv_creator_count(OLD.creator_id);
+        RETURN OLD;
+    END CASE;
+  END;
+  $block1$ LANGUAGE PLPGSQL;
+
+  RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: DONE setting up procedures/functions for update of the creator.course_count.';
+END
+$block$ LANGUAGE PLPGSQL;
+
