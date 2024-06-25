d5820a4e1d4b1a790184b9659fe118f321d95fa2
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-25 14:40:36 +0100

    removed the ability to teardown and setup the db before creating superuser (will be handled by the reset script)

--- console/hard-start.sh
+++ console/hard-start.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-# this is the startup script for the console application in dev mode with db teardown and setup
+# this is the startup script for the console application in dev mode. it creates superusers before interacting with DB
 
 source ../db/.environment.zshrc &&\
 source ./.console.zshrc &&\
-cd ../db && ./setup.sh &&\
-	psql -Utoughware -hlocalhost -dcourseta -c "INSERT INTO admins (admin_id, email, password, is_superuser) VALUES ('$CST_SUPERUSER_ID' , '$CST_SUPERUSER_EMAIL', '$CST_SUPERUSER_PASSWORD', 't') ON CONFLICT DO NOTHING;" &&\
-	psql -Utoughware -hlocalhost -dcourseta -c "INSERT INTO creators (creator_id ,email, first_name, last_name, password)\
-	VALUES ('$CST_SUPERUSER_ID','$CST_SUPERUSER_EMAIL' , '$CST_SUPERUSER_FIRSTNAME', '$CST_SUPERUSER_LASTNAME', '$CST_SUPERUSER_PASSWORD') ON CONFLICT DO NOTHING;" &&\
-cd - &&\
+psql -Utoughware -hlocalhost -dcourseta -c "INSERT INTO admins (admin_id, email, password, is_superuser) VALUES ('$CST_SUPERUSER_ID' , '$CST_SUPERUSER_EMAIL', '$CST_SUPERUSER_PASSWORD', 't') ON CONFLICT DO NOTHING;" &&\
+psql -Utoughware -hlocalhost -dcourseta -c "INSERT INTO creators (creator_id ,email, first_name, last_name, password)\
+VALUES ('$CST_SUPERUSER_ID','$CST_SUPERUSER_EMAIL' , '$CST_SUPERUSER_FIRSTNAME', '$CST_SUPERUSER_LASTNAME', '$CST_SUPERUSER_PASSWORD') ON CONFLICT DO NOTHING;" &&\
 npm run dev -- "-e${CST_SUPERUSER_EMAIL}" "-p${CST_SUPERUSER_PASSWORD}" "-i${CST_SUPERUSER_ID}"
 
