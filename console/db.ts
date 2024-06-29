1e9bc541e04bc2f1349c1ff26cddd2e8dc26c922
HazelDaniel <hazeldaniel856@gmail.com>
2024-06-29 10:45:12 +0100

    added context awareness to the database connections

--- console/db.ts
+++ console/db.ts
@@ -1,18 +1,21 @@
 import { config } from "dotenv";
-config({ path: [".env", ".env.dev"] });
+const envPaths = [".env", ".env.dev"];
+config({ path: envPaths });
 import pg from "pg";
 const { Pool } = pg;
 
 export const pool = new Pool({
   host: process.env.CST_DB_HOST,
   user: process.env.CST_DB_USER,
-  database: process.env.CST_DB_NAME,
+  database:
+    process.env.CST_CONTEXT === "test"
+      ? process.env.CST_TEST_DB_NAME
+      : process.env.CST_DB_NAME,
   max: 20,
   password: process.env.CST_DB_PASSWORD,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 50000,
 });
-
 
 process.on("SIGINT", async () => {
   await pool.end(); // Close idle connections in the pool on termination
