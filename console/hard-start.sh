c1856d20df4f10ce753b6dec5022f6372576461b
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-02 03:14:13 +0100

    instruct hard-start script to run in dev mode only if the context is not test, so that we can populate the db with necessary credentials first before running the test and without opening console

--- console/hard-start.sh
+++ console/hard-start.sh
@@ -6,5 +6,10 @@ source ./.console.zshrc &&\
 psql -Utoughware -hlocalhost -dcourseta -c "INSERT INTO admins (admin_id, email, password, is_superuser) VALUES ('$CST_SUPERUSER_ID' , '$CST_SUPERUSER_EMAIL', '$CST_SUPERUSER_PASSWORD', 't') ON CONFLICT DO NOTHING;" &&\
 psql -Utoughware -hlocalhost -dcourseta -c "INSERT INTO creators (creator_id ,email, first_name, last_name, password)\
 VALUES ('$CST_SUPERUSER_ID','$CST_SUPERUSER_EMAIL' , '$CST_SUPERUSER_FIRSTNAME', '$CST_SUPERUSER_LASTNAME', '$CST_SUPERUSER_PASSWORD') ON CONFLICT DO NOTHING;" &&\
-npm run dev -- "-e${CST_SUPERUSER_EMAIL}" "-p${CST_SUPERUSER_PASSWORD}" "-i${CST_SUPERUSER_ID}"
+psql -Utoughware -hlocalhost -dcourseta_test -c "INSERT INTO admins (admin_id, email, password, is_superuser) VALUES ('$CST_SUPERUSER_ID' , '$CST_SUPERUSER_EMAIL', '$CST_SUPERUSER_PASSWORD', 't') ON CONFLICT DO NOTHING;" &&\
+psql -Utoughware -hlocalhost -dcourseta_test -c "INSERT INTO creators (creator_id ,email, first_name, last_name, password)\
+VALUES ('$CST_SUPERUSER_ID','$CST_SUPERUSER_EMAIL' , '$CST_SUPERUSER_FIRSTNAME', '$CST_SUPERUSER_LASTNAME', '$CST_SUPERUSER_PASSWORD') ON CONFLICT DO NOTHING;" &&\
 
+if [[ -z $1 ]]; then
+./dev
+fi
