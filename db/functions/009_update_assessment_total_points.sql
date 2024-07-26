62ce7fa83dd595a9524972c3abd5686a680f321f
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-26 07:52:54 +0100

    updated the attributes of the concete assessments classes instead of the inherited abstract class, renamed functions for ordering in the db dump

--- db/functions/009_update_assessment_total_points.sql
+++ db/functions/009_update_assessment_total_points.sql
@@ -3,42 +3,54 @@ $block$
 BEGIN
   -- RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for update of the assessment.total_points.';
 
-  CREATE OR REPLACE PROCEDURE update_question_equiv_assessment_points
-  (assessment_id_ UUID, new_point INT) LANGUAGE PLPGSQL AS
+  CREATE OR REPLACE PROCEDURE p_01_update_question_equiv_assessment_points
+  (assessment_id_ UUID, new_point INT, assessment_type_ courseta.ASSESSMENT_TYPE) LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
-    -- RAISE NOTICE '[debug]: updating with the new point: %', new_point;
-    UPDATE assessments SET total_points = (total_points + new_point)
-		WHERE assessments.assessment_id = assessment_id_;
+    CASE assessment_type_
+      WHEN 'quiz' THEN
+        UPDATE coursetae.quizzes SET total_points = (total_points + new_point)
+        WHERE quizzes.assessment_id = assessment_id_;
+      ELSE
+        UPDATE courseta.exams SET total_points = (total_points + new_point)
+        WHERE exams.assessment_id = assessment_id_;
+    END CASE;
   END;
   $block2$;
 
-  CREATE OR REPLACE FUNCTION update_points_and_question_count_on_assessment () RETURNS TRIGGER AS
+  CREATE OR REPLACE FUNCTION p_02_update_points_and_question_count_on_assessment () RETURNS TRIGGER AS
   $block1$
   BEGIN
-    CALL update_question_equiv_assessment_points(NEW.assessment_id, NEW.points - OLD.points);
+    CALL p_01_update_question_equiv_assessment_points(NEW.assessment_id, NEW.points - OLD.points, NEW.assessment_type);
 		RETURN NEW;
   END;
   $block1$ LANGUAGE PLPGSQL;
 
   -- RAISE NOTICE '<[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for addition to the assessment.total_points.';
 
-  CREATE OR REPLACE PROCEDURE add_question_equiv_assessment_points_and_count
-  (assessment_id_ UUID, new_point INT) LANGUAGE PLPGSQL AS
+  CREATE OR REPLACE PROCEDURE p_01_add_question_equiv_assessment_points_and_count
+  (assessment_id_ UUID, new_point INT, assessment_type_ courseta.ASSESSMENT_TYPE) LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
     -- RAISE NOTICE '[debug]: adding with the new point: %', new_point;
-    UPDATE assessments SET total_points = (total_points + new_point),
-    question_count = (question_count + 1)
-		WHERE assessments.assessment_id = assessment_id_;
+    CASE assessment_type_
+      WHEN 'quiz' THEN
+        UPDATE courseta.quizzes SET total_points = (total_points + new_point),
+        question_count = (question_count + 1)
+        WHERE quizzes.assessment_id = assessment_id_;
+      ELSE
+        UPDATE courseta.exams SET total_points = (total_points + new_point),
+        question_count = (question_count + 1)
+        WHERE exams.assessment_id = assessment_id_;
+    END CASE;
   END;
   $block2$;
 
 
-  CREATE OR REPLACE FUNCTION add_points_and_question_count_to_assessment () RETURNS TRIGGER AS
+  CREATE OR REPLACE FUNCTION p_02_add_points_and_question_count_to_assessment () RETURNS TRIGGER AS
   $block1$
   BEGIN
-    CALL add_question_equiv_assessment_points_and_count(NEW.assessment_id, NEW.points);
+    CALL p_01_add_question_equiv_assessment_points_and_count(NEW.assessment_id, NEW.points, NEW.assessment_type);
 		RETURN NEW;
   END;
   $block1$ LANGUAGE PLPGSQL;
@@ -48,21 +60,28 @@ BEGIN
 
   -- RAISE NOTICE '<[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for subtraction from the assessment.total_points.';
 
-  CREATE OR REPLACE FUNCTION remove_points_and_question_count_from_assessment () RETURNS TRIGGER AS
+  CREATE OR REPLACE FUNCTION p_02_remove_points_and_question_count_from_assessment () RETURNS TRIGGER AS
   $block1$
   BEGIN
-    CALL subtract_question_equiv_assessment_points_and_count(OLD.assessment_id, OLD.points);
+    CALL p_01_subtract_question_equiv_assessment_points_and_count(OLD.assessment_id, OLD.points, NEW.assessment_type);
 		RETURN OLD;
   END;
   $block1$ LANGUAGE PLPGSQL;
 
-  CREATE OR REPLACE PROCEDURE subtract_question_equiv_assessment_points_and_count
-  (assessment_id_ UUID, old_point INT) LANGUAGE PLPGSQL AS
+  CREATE OR REPLACE PROCEDURE p_01_subtract_question_equiv_assessment_points_and_count
+  (assessment_id_ UUID, old_point INT, assessment_type_ courseta.ASSESSMENT_TYPE) LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
-    UPDATE assessments SET total_points = (total_points - old_point),
-    question_count = (question_count - 1)
-		WHERE assessments.assessment_id = assessment_id_;
+    CASE assessment_type_
+      WHEN 'quiz' THEN
+        UPDATE courseta.quizzes SET total_points = (total_points - old_point),
+        question_count = (question_count - 1)
+        WHERE quizzes.assessment_id = assessment_id_;
+      ELSE
+        UPDATE courseta.exams SET total_points = (total_points - old_point),
+        question_count = (question_count - 1)
+        WHERE exams.assessment_id = assessment_id_;
+    END CASE;
   END;
   $block2$;
 
