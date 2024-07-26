bf86130132e9e8feb6e0b3479f640f09cacd28be
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-26 08:00:10 +0100

    updated the functions to work with the attribute update on the courses table

--- db/SET-functions/update_course_attributes.sql
+++ db/SET-functions/update_course_attributes.sql
@@ -6,19 +6,29 @@ BEGIN
   RAISE NOTICE '<[SETUP]   (SET) FUNCTION: setting up the update_course_attributes function ...';
 
   CREATE OR REPLACE FUNCTION update_course_attributes (course_data JSONB)
-  RETURNS TABLE (thumbnail TEXT, description TEXT, tags VARCHAR[]) AS
+  RETURNS TABLE (url TEXT, description TEXT, tags VARCHAR[]) AS
   $block1$
   DECLARE
     new_tags       VARCHAR[] DEFAULT ARRAY[]::VARCHAR[];
+    old_avatar_url                                 TEXT;
   BEGIN
     new_tags := ARRAY(SELECT jsonb_array_elements_text(course_data->'tags'));
 
+    SELECT INTO old_avatar_url courses.avatar->>'url' FROM courseta.courses
+    WHERE courses.course_id = (course_data->>'courseID')::BIGINT;
+
+		IF course_data->>'thumbnail' <> old_avatar_url THEN
+			UPDATE courseta.courses
+      SET avatar = avatar || ('{"url": "' || (course_data->>'thumbnail')::TEXT || '", "created_at": "' ||  courses.created_at::TEXT || '", "updated_at": "' || CURRENT_TIMESTAMP::TEXT || '"}')::JSONB
+      WHERE course_id = (course_data->>'courseID')::BIGINT;
+		END IF;
+
     RETURN QUERY 
     UPDATE courseta.courses
-    SET thumbnail = COALESCE(course_data->>'thumbnail', courses.thumbnail), description = COALESCE(course_data->>'description', courses.description),
+    SET description = COALESCE(course_data->>'description', courses.description),
     tags = COALESCE(NULLIF(new_tags::VARCHAR[], ARRAY[]::VARCHAR[]), courses.tags)
     WHERE course_id = (course_data->>'courseID')::BIGINT
-    RETURNING courses.thumbnail, courses.description, courses.tags;
+    RETURNING courses.avatar->>'url', courses.description, courses.tags;
   END;
   $block1$ LANGUAGE PLPGSQL;
 
