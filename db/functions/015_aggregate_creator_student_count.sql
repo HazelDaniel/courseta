41dd9f1943f67e3ce82b21561696f4702afe1d92
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-26 07:51:27 +0100

    updated the algorithm for fetching distinct course enrolment for creators, renamed functions for ordering in the db dump

--- db/functions/015_aggregate_creator_student_count.sql
+++ db/functions/015_aggregate_creator_student_count.sql
@@ -3,31 +3,46 @@ $block$
 BEGIN
   RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for update of the creator.student_count.';
 
-  CREATE OR REPLACE PROCEDURE update_student_equiv_creator_count
-  (creator_id_ UUID) LANGUAGE PLPGSQL AS
+  CREATE OR REPLACE PROCEDURE p_01_update_student_equiv_creator_count
+  (course_id_ BIGINT) LANGUAGE PLPGSQL AS
   $block2$
   DECLARE
     tot_student_count         INT;
+    equiv_creator_id_        UUID;
   BEGIN
+    SELECT INTO equiv_creator_id_ creator_id FROM courseta.courses
+    WHERE course_id = course_id_;
+    -- RAISE NOTICE '[debug]: updating the student count on creator';
 
-    RAISE NOTICE '[debug]: updating the student count on creator';
-
-    SELECT INTO tot_student_count SUM (student_count) 
-    FROM courseta.courses
-    WHERE courses.creator_id = creator_id_;
+    WITH distinct_enrollment_on_course AS (
+      SELECT ROW_NUMBER() OVER (PARTITION BY students__courses.student_id), students__courses.student_id, students__courses.course_id FROM
+      courseta.students__courses
+      JOIN courseta.courses USING (course_id)
+      WHERE courses.creator_id = equiv_creator_id_
+    )
+    SELECT INTO tot_student_count COUNT(*) FROM distinct_enrollment_on_course WHERE row_number = 1;
 
     UPDATE creators SET student_count = tot_student_count
-		WHERE creators.creator_id = creator_id_;
+		WHERE creators.creator_id = equiv_creator_id_;
   END;
   $block2$;
 
-  CREATE OR REPLACE FUNCTION update_student_count_on_creator () RETURNS TRIGGER AS
+  CREATE OR REPLACE FUNCTION p_02_update_student_count_on_creator () RETURNS TRIGGER AS
   $block1$
   BEGIN
-    IF NEW.student_count <> OLD.student_count THEN
-      CALL update_student_equiv_creator_count(NEW.creator_id);
-    END IF;
-    RETURN NEW;
+
+    CASE TG_OP
+      WHEN 'INSERT' THEN
+          CALL p_01_update_student_equiv_creator_count(NEW.course_id);
+          RETURN NEW;
+      WHEN 'DELETE' THEN
+          CALL p_01_update_student_equiv_creator_count(OLD.course_id);
+          RETURN OLD;
+      ELSE
+        -- RAISE EXCEPTION 'cannot perform any other operation other than insert and delete %', TG_OP;
+        RETURN NEW;
+      END CASE;
+
   END;
   $block1$ LANGUAGE PLPGSQL;
 
