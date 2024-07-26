3d040da045a7241aa7c2aeac94faaacaf989c344
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-26 07:39:56 +0100

    renamed functions for correct ordering in the db dump

--- db/functions/010_aggregate_lesson_total_duration.sql
+++ db/functions/010_aggregate_lesson_total_duration.sql
@@ -3,7 +3,7 @@ $block$
 BEGIN
   -- RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for update of the lesson.total_duration.';
 
-  CREATE OR REPLACE PROCEDURE update_lesson_content_lesson_dur_and_count
+  CREATE OR REPLACE PROCEDURE p_01_update_lesson_content_lesson_dur_and_count
   (lesson_id_ BIGINT, duration INT, increment_by SMALLINT) LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
@@ -13,20 +13,20 @@ BEGIN
   END;
   $block2$;
 
-  CREATE OR REPLACE FUNCTION update_lesson_duration_and_content_count () RETURNS TRIGGER AS
+  CREATE OR REPLACE FUNCTION p_02_update_lesson_duration_and_content_count () RETURNS TRIGGER AS
   $block1$
   BEGIN
     CASE TG_OP
       WHEN 'UPDATE' THEN
         IF NEW.duration <> OLD.duration THEN
-          CALL update_lesson_content_lesson_dur_and_count(NEW.lesson_id, NEW.duration - OLD.duration, 0::SMALLINT);
+          CALL p_01_update_lesson_content_lesson_dur_and_count(NEW.lesson_id, NEW.duration - OLD.duration, 0::SMALLINT);
         END IF;
         RETURN NEW;
       WHEN 'INSERT' THEN
-          CALL update_lesson_content_lesson_dur_and_count(NEW.lesson_id, NEW.duration, 1::SMALLINT);
+          CALL p_01_update_lesson_content_lesson_dur_and_count(NEW.lesson_id, NEW.duration, 1::SMALLINT);
         RETURN NEW;
       WHEN 'DELETE' THEN
-        CALL update_lesson_content_lesson_dur_and_count(OLD.lesson_id, OLD.duration, -1::SMALLINT);
+        CALL p_01_update_lesson_content_lesson_dur_and_count(OLD.lesson_id, OLD.duration, -1::SMALLINT);
         RETURN OLD;
     END CASE;
   END;
