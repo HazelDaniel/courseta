761c6865a7d7ac231b07245210dfbf98e54de5e6
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-26 07:36:34 +0100

    renamed functions for correct ordering in the db dump

--- db/functions/001_insert_corresponding_user.sql
+++ db/functions/001_insert_corresponding_user.sql
@@ -3,7 +3,7 @@ $block$
 BEGIN
   RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for user insertion.';
 
-  CREATE OR REPLACE PROCEDURE insert_user_with_student_id (student_id UUID)
+  CREATE OR REPLACE PROCEDURE p_01_insert_user_with_student_id (student_id UUID)
   LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
@@ -11,7 +11,7 @@ BEGIN
   END;
   $block2$;
 
-  CREATE OR REPLACE PROCEDURE insert_user_with_creator_id (creator_id UUID, role_ courseta.USER_ROLE_TYPE)
+  CREATE OR REPLACE PROCEDURE p_01_insert_user_with_creator_id (creator_id UUID, role_ courseta.USER_ROLE_TYPE)
   LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
@@ -19,18 +19,18 @@ BEGIN
   END;
   $block2$;
 
-  CREATE OR REPLACE FUNCTION insert_student_equiv_user () RETURNS TRIGGER AS
+  CREATE OR REPLACE FUNCTION p_02_insert_student_equiv_user () RETURNS TRIGGER AS
   $block1$
     BEGIN
-      CALL insert_user_with_student_id(NEW.student_id);
+      CALL p_01_insert_user_with_student_id(NEW.student_id);
       RETURN NEW;
     END;
   $block1$ LANGUAGE PLPGSQL;
 
-  CREATE OR REPLACE FUNCTION insert_creator_equiv_user () RETURNS TRIGGER AS
+  CREATE OR REPLACE FUNCTION p_02_insert_creator_equiv_user () RETURNS TRIGGER AS
   $block3$
     BEGIN
-      CALL insert_user_with_creator_id(NEW.creator_id, 'creator'::courseta.USER_ROLE_TYPE);
+      CALL p_01_insert_user_with_creator_id(NEW.creator_id, 'creator'::courseta.USER_ROLE_TYPE);
       RETURN NEW;
     END;
   $block3$ LANGUAGE PLPGSQL;
