48766dc4c3b4261f67c66587fcc7d3f5a60117fb
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-26 07:37:04 +0100

    renamed functions for correct ordering in the db dump

--- db/functions/003_insert_corresponding_assessment.sql
+++ db/functions/003_insert_corresponding_assessment.sql
@@ -3,7 +3,7 @@ $block$
 BEGIN
   -- RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for assessment insertion.';
 
-  CREATE OR REPLACE PROCEDURE insert_assessment_with_exam_id (exam_id UUID, exam_description TEXT, assessment_type courseta.ASSESSMENT_TYPE)
+  CREATE OR REPLACE PROCEDURE p_01_insert_assessment_with_exam_id (exam_id UUID, exam_description TEXT, assessment_type courseta.ASSESSMENT_TYPE)
   LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
@@ -11,16 +11,16 @@ BEGIN
   END;
   $block2$;
 
-  CREATE OR REPLACE FUNCTION insert_exam_equiv_assessment () RETURNS TRIGGER AS
+  CREATE OR REPLACE FUNCTION p_02_insert_exam_equiv_assessment () RETURNS TRIGGER AS
   $block1$
     BEGIN
-      CALL insert_assessment_with_exam_id(NEW.exam_id, NEW.description, 'exam');
+      CALL p_01_insert_assessment_with_exam_id(NEW.exam_id, NEW.description, 'exam');
       RETURN NEW;
     END;
   $block1$ LANGUAGE PLPGSQL;
 
 
-  CREATE OR REPLACE PROCEDURE insert_assessment_with_quiz_id (quiz_id UUID, quiz_description TEXT)
+  CREATE OR REPLACE PROCEDURE p_01_insert_assessment_with_quiz_id (quiz_id UUID, quiz_description TEXT)
   LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
@@ -29,10 +29,10 @@ BEGIN
   $block2$;
   -- RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: DONE setting up procedures/functions for assessment insertion.';
 
-  CREATE OR REPLACE FUNCTION insert_quiz_equiv_assessment () RETURNS TRIGGER AS
+  CREATE OR REPLACE FUNCTION p_02_insert_quiz_equiv_assessment () RETURNS TRIGGER AS
   $block3$
     BEGIN
-      CALL insert_assessment_with_quiz_id(NEW.quiz_id, NEW.description);
+      CALL p_01_insert_assessment_with_quiz_id(NEW.quiz_id, NEW.description);
       RETURN NEW;
     END;
   $block3$ LANGUAGE PLPGSQL;
