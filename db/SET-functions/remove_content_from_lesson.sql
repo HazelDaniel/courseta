ab58419c542428ed8a3dd2d73f582952e26d4a81
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-26 18:06:58 +0100

    added a SET function to remove content from lesson

---
+++ db/SET-functions/remove_content_from_lesson.sql
@@ -0,0 +1,19 @@
+DO
+$block$
+BEGIN
+  RAISE NOTICE '[SETUP]   (SET) FUNCTION: setting up the remove_content_from_lesson function ...';
+
+  CREATE OR REPLACE PROCEDURE remove_content_from_lesson (lesson_id_ BIGINT)
+  LANGUAGE PLPGSQL AS
+  $block1$
+  BEGIN
+    DELETE from courseta.lesson_contents WHERE lesson_contents.lesson_id = lesson_id_;
+  END;
+  $block1$;
+
+  RAISE NOTICE '[SETUP]   (SET) FUNCTION: DONE setting up the remove_content_from_lesson function.';
+END
+$block$ LANGUAGE PLPGSQL;
+
+
+
