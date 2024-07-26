f519f4b08a4a9a5c3b008dfd968701b01465aa7a
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-26 07:50:39 +0100

    added default values for possible null calculations, renamed functions for ordering in the db dump

--- db/functions/012_aggregate_course_review.sql
+++ db/functions/012_aggregate_course_review.sql
@@ -3,7 +3,7 @@ $block$
 BEGIN
   -- RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for update of the course.[review_attribute].';
 
-  CREATE OR REPLACE PROCEDURE update_average_rating_on_course (course_id_ BIGINT)
+  CREATE OR REPLACE PROCEDURE p_01_update_average_rating_on_course (course_id_ BIGINT)
   LANGUAGE PLPGSQL AS
   $block1$
   DECLARE
@@ -13,14 +13,14 @@ BEGIN
     courseta.reviews WHERE course_id = course_id_;
 
     UPDATE courseta.courses SET
-    average_rating = COALESCE(avg_rating::NUMERIC(2, 1), 0)
+    average_rating = COALESCE(avg_rating::NUMERIC(2, 1), 5.0)
     WHERE courses.course_id = course_id_;
   END;
   $block1$;
 
   -- RAISE NOTICE '<[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for addition to the course.review_count.';
 
-  CREATE OR REPLACE PROCEDURE add_review_equiv_course_count
+  CREATE OR REPLACE PROCEDURE p_01_add_review_equiv_course_count
   (course_id_ BIGINT) LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
@@ -33,7 +33,7 @@ BEGIN
 
   -- RAISE NOTICE '<[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for deduction from the course.[review attribute].';
 
-  CREATE OR REPLACE PROCEDURE subtract_review_equiv_course_count
+  CREATE OR REPLACE PROCEDURE p_01_subtract_review_equiv_course_count
   (course_id_ BIGINT) LANGUAGE PLPGSQL AS
   $block2$
   BEGIN
@@ -44,21 +44,21 @@ BEGIN
 
   -- RAISE NOTICE '<[SETUP]  PROCEDURE/FUNCTION: DONE setting up procedures/functions for deducation from the course.[review attribute].';
 
-  CREATE OR REPLACE FUNCTION update_review_on_course () RETURNS TRIGGER AS
+  CREATE OR REPLACE FUNCTION p_02_update_review_on_course () RETURNS TRIGGER AS
   $block1$
   BEGIN
     CASE TG_OP
       WHEN 'UPDATE' THEN
-          CALL update_average_rating_on_course(NEW.course_id);
+          CALL p_01_update_average_rating_on_course(NEW.course_id);
           RETURN NEW;
       WHEN 'INSERT' THEN
         -- RAISE NOTICE '[debug]: adding review instead with %', NEW.rating;
-        CALL update_average_rating_on_course(NEW.course_id);
-        CALL add_review_equiv_course_count(NEW.course_id);
+        CALL p_01_update_average_rating_on_course(NEW.course_id);
+        CALL p_01_add_review_equiv_course_count(NEW.course_id);
         RETURN NEW;
       WHEN 'DELETE' THEN
-        CALL update_average_rating_on_course(OLD.course_id);
-        CALL subtract_review_equiv_course_count(OLD.course_id);
+        CALL p_01_update_average_rating_on_course(OLD.course_id);
+        CALL p_01_subtract_review_equiv_course_count(OLD.course_id);
         RETURN OLD;
     END CASE;
   END;
