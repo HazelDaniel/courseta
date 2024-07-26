4b7c07bdfe56ffc3bfdf632a328164e3eea691fa
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-26 07:42:09 +0100

    renamed functions for correct ordering in the db dump

--- db/functions/014_aggregate_creator_course_review_count.sql
+++ db/functions/014_aggregate_creator_course_review_count.sql
@@ -3,7 +3,7 @@ $block$
 BEGIN
   -- RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for update of the creator.course_review_count.';
 
-  CREATE OR REPLACE PROCEDURE update_course_review_equiv_creator_count
+  CREATE OR REPLACE PROCEDURE p_01_update_course_review_equiv_creator_count
   (creator_id_ UUID) LANGUAGE PLPGSQL AS
   $block2$
   DECLARE
@@ -16,20 +16,23 @@ BEGIN
 		GROUP BY creator_id) as RES;
 
     UPDATE courseta.creators
-    SET course_review_count = tot_review.review_count_,
-    average_course_rating = tot_review.avg_rating::NUMERIC(2, 1)
+    SET course_review_count = COALESCE(tot_review.review_count_, 0),
+    average_course_rating = COALESCE(tot_review.avg_rating::NUMERIC(2, 1), 5.0)
     WHERE creators.creator_id = creator_id_;
   END;
   $block2$;
 
-  CREATE OR REPLACE FUNCTION update_course_review_count_on_creator () RETURNS TRIGGER AS
+  CREATE OR REPLACE FUNCTION p_02_update_course_review_count_on_creator () RETURNS TRIGGER AS
   $block1$
   BEGIN
-    IF NEW.review_count <> OLD.review_count OR NEW.student_count <> OLD.student_count OR NEW.average_rating <> OLD.average_rating THEN
-      CALL update_course_review_equiv_creator_count(NEW.creator_id);
+    IF NEW.review_count <> OLD.review_count
+    OR NEW.student_count <> OLD.student_count
+    OR NEW.average_rating <> OLD.average_rating THEN
+      CALL p_01_update_course_review_equiv_creator_count(NEW.creator_id);
     END IF;
 
 		IF TG_OP = 'DELETE' THEN
+      CALL p_01_update_course_review_equiv_creator_count(OLD.creator_id);
 			RETURN OLD;
 		END IF;
 
