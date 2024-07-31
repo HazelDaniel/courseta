089a68885aff070cee461e48f2b1346ec223fbb2
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-31 07:15:28 +0100

    added the setup script (bash)

---
+++ image-db/setup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# db teardown, cleanup and setup
+source ./.environment.zshrc
+
+
+for i in $(find ./setup -type f | sort -n); do
+	psql -Utoughware -p5432 -hlocalhost -d postgres -f "$i";
+done
+
+
+if [[ $? == 0 ]]; then
+	for i in $(find ./tables -type f | sort -n); do
+		if [[ $? == 0 ]]; then
+			psql -Utoughware -p5432 -hlocalhost -d courseta_image -f "$i" &&\
+			echo '---------------------------------------------------------------------------------------------------' &&\
+			psql -Utoughware -p5432 -hlocalhost -d courseta_image_test -f "$i";
+		else
+			break;
+		fi
+	done
+fi
+
+if [[ $? == 0 ]]; then
+	for i in $(find ./functions -type f |  sort -n); do
+		if [[ $? == 0 ]]; then
+			psql -Utoughware -p5432 -hlocalhost -d courseta_image -f "$i" &&\
+			echo '---------------------------------------------------------------------------------------------------' &&\
+			psql -Utoughware -p5432 -hlocalhost -d courseta_image_test -f "$i";
+		else
+			break;
+		fi
+	done
+fi
