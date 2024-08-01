0b9726852260052ac1c20a3dbf331e94d46b1d16
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-01 19:18:26 +0100

    mounted children routes under the root route

--- server/api/v1/routes/root.route.ts
+++ server/api/v1/routes/root.route.ts
@@ -1,32 +1,20 @@
+import { v1AssessmentsRouter } from './assessments.route.js';
+import { v1StudentsRouter } from './students.route.js';
+import { v1CoursesRouter } from "./courses.route.js";
+import { v1CreatorsRouter } from "./creators.route.js";
 import express, { NextFunction, Request, Response } from "express";
 export const v1Router = express.Router();
+
+v1Router.use("/creators", v1CreatorsRouter);
+v1Router.use("/courses", v1CoursesRouter);
+v1Router.use("/students", v1StudentsRouter);
+v1Router.use("/assessments", v1AssessmentsRouter);
 
 v1Router.use((req, res, next) => {
   console.log(`[${req.method}]: ${req.baseUrl}${req.url}`);
   next();
 });
 
-v1Router.get("/courses", (req, res) => {
-  return res.status(200).json({ message: "welcome to the courses route" });
-});
-
-v1Router.get("/creators/:creator_id/courses", (req, res) => {
-  return res
-    .status(200)
-    .json({ message: "welcome to the creator courses route" });
-});
-
-v1Router.get("/creators/:creator_id/me", (req, res) => {
-  return res
-    .status(200)
-    .json({ message: "welcome to the creator profile route" });
-});
-
-v1Router.get("/creators/:creator_id/me", (req, res) => {
-  return res
-    .status(200)
-    .json({ message: "welcome to the creator profile route" });
-});
 
 v1Router.get("/", (req, res) => {
   return res.status(200).json({ message: "welcome" });
