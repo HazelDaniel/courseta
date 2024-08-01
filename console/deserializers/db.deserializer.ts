c06629d4577100251b0147172e253b65e45cb00d
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-01 08:09:09 +0100

    added deserializers to convert db data to application-compatible data

---
+++ console/deserializers/db.deserializer.ts
@@ -0,0 +1,13 @@
+import { base64toDataURL } from "../utils.js";
+
+export const deserializeImage: (
+  base64String: string,
+  mimetype: string
+) => string | null = (base64String, mimeType) => {
+  // console.log("before deserialization:------------ ");
+  // console.log(base64String, "mime-type: ", mimeType);
+  const deserializedToken = base64toDataURL(base64String, mimeType);
+  // console.log("after deserialization:------------ ");
+  // console.log(deserializedToken);
+  return deserializedToken;
+};
