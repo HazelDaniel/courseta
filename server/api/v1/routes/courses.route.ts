8cb331c926c2a825e0cd54b8c75f116bf8f0b1e7
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-02 07:57:04 +0100

    added the generic courses endpoint

--- server/api/v1/routes/courses.route.ts
+++ server/api/v1/routes/courses.route.ts
@@ -1,6 +1,9 @@
+import { CourseModel } from "../../../models/v1/course.model.js";
 import express from "express";
 export const v1CoursesRouter = express.Router();
 
-v1CoursesRouter.get("/", (req, res) => {
-  return res.status(200).json({ message: "welcome to the courses route" });
+v1CoursesRouter.get("/", async (req, res) => {
+  const resCourses = await CourseModel.all();
+  return res.status(200).json(resCourses);
 });
+
