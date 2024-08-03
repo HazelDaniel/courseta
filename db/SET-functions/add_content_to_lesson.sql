f99652628b7baed97f084ab520dfd6574056ff72
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-03 18:57:31 +0100

    updated the set function to return a content id

--- db/SET-functions/add_content_to_lesson.sql
+++ db/SET-functions/add_content_to_lesson.sql
@@ -1,9 +1,11 @@
 DO
 $block$
 BEGIN
-CREATE OR REPLACE FUNCTION add_content_to_lesson (p_lesson_id BIGINT, p_title TEXT, p_href TEXT, p_content_type courseta.LESSON_CONTENT_TYPE, p_duration INT) RETURNS VOID
+CREATE OR REPLACE FUNCTION add_content_to_lesson (p_lesson_id BIGINT, p_title TEXT, p_href TEXT, p_content_type courseta.LESSON_CONTENT_TYPE, p_duration INT) RETURNS BIGINT
 AS
 $block1$
+  DECLARE
+    result_content_id        BIGINT;
   BEGIN
     INSERT INTO courseta.lesson_contents(lesson_id, title, href, content_type, duration)
     VALUES (
@@ -12,7 +14,9 @@ $block1$
       p_href,
       p_content_type::courseta.LESSON_CONTENT_TYPE,
       p_duration::INT
-    );
+    ) RETURNING lesson_content_id INTO result_content_id;
+
+    RETURN result_content_id;
 
   EXCEPTION
     WHEN others THEN
