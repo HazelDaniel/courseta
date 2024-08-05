cc859db1a5b6c3bf89577765be25320db1553c77
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-05 09:36:15 +0100

    ensured uniformity in the way the responses are sent

--- server/api/v1/routes/courses.route.ts
+++ server/api/v1/routes/courses.route.ts
@@ -1,9 +1,13 @@
 import { CourseModel } from "../../../models/v1/course.model.js";
 import express from "express";
+import { ServerPayloadType } from "../../../types.js";
 export const v1CoursesRouter = express.Router();
 
 v1CoursesRouter.get("/", async (req, res) => {
   const resCourses = await CourseModel.all();
-  return res.status(200).json(resCourses);
+  const resPayload: ServerPayloadType<typeof resCourses> = {
+    payload: resCourses,
+    message: null,
+  };
+  return res.status(200).json(resPayload);
 });
-
