d9a5a10e86a54067440172570d44830102dbe928
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-05 09:53:02 +0100

    added middlewares for access control and auth

---
+++ server/api/v1/middlewares/auth.middleware.ts
@@ -0,0 +1,25 @@
+import type { NextFunction, Request, Response } from "express";
+import { ServerError } from "../../../utils.js";
+
+export const localProtected = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.isAuthenticated())
+    next(
+      new ServerError("only authenticated creators can access this route", 401)
+    );
+  next();
+};
+
+export const creatorIDProtected = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { creator_id: creatorID } = req.params;
+  if ((req.user as any).id !== creatorID)
+    next(new ServerError("you are not allowed to access this route!", 403));
+  next();
+};
