be4091db3f1712859948e5caa22f478513a4ddf3
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-06 21:07:17 +0100

    added a SET function to archive a course for creator

---
+++ db/SET-functions/archive_course_for_creator.sql
@@ -0,0 +1,30 @@
+DO
+$block$
+BEGIN
+  RAISE NOTICE '[SETUP]   (SET) FUNCTION: setting up the archive_course_for_creator function ...';
+
+  CREATE OR REPLACE FUNCTION archive_course_for_creator (course_id_ BIGINT, creator_id_ UUID) RETURNS
+  VOID AS
+  $block1$
+  DECLARE
+    equiv_creator_id      UUID;
+  BEGIN
+
+    SELECT INTO equiv_creator_id courses.creator_id
+    FROM courseta.courses
+    WHERE courses.course_id = course_id_;
+
+    IF equiv_creator_id <> creator_id_ THEN
+      RAISE EXCEPTION 'you cannot archive a course you didn''t create!';
+    END IF;
+
+    UPDATE courseta.courses
+    SET archived = true
+    WHERE courses.course_id = course_id_;
+
+  END;
+  $block1$ LANGUAGE PLPGSQL;
+
+  RAISE NOTICE '[SETUP]   (SET) FUNCTION: DONE setting up the archive_course_for_creator function.';
+END;
+$block$ LANGUAGE PLPGSQL;
