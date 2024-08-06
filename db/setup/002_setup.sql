e5446a340561785d8cecc3f0975cf7f3952dccf9
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-06 21:07:51 +0100

    added the setup script for my server authentication session store

---
+++ db/setup/002_setup.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS courseta_users_session;
+CREATE DATABASE courseta_users_session;
+
+DROP DATABASE IF EXISTS courseta_users_session_test;
+CREATE DATABASE courseta_users_session_test;
