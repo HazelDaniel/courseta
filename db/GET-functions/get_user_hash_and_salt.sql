5e343d2dd3115deabc423bb21dc9c1459322b14b
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-06 21:06:46 +0100

    added a GET function to retrieve a users hash and salt

---
+++ db/GET-functions/get_user_hash_and_salt.sql
@@ -0,0 +1,26 @@
+DO
+$block$
+BEGIN
+  RAISE NOTICE '[SETUP]   (SET) FUNCTION: setting up functions that retrieves user password.';
+
+  CREATE OR REPLACE FUNCTION get_user_hash_and_salt (
+    user_id_ UUID,
+    type_ courseta.USER_ROLE_TYPE
+  )
+  RETURNS TABLE(hash TEXT, salt TEXT) AS
+  $block1$
+  DECLARE
+  BEGIN
+    CASE type_
+    WHEN 'student' THEN
+      RETURN QUERY SELECT password hash, students.salt FROM courseta.students WHERE student_id = user_id_;
+    WHEN 'creator' THEN
+      RETURN QUERY SELECT password hash, creators.salt FROM courseta.creators WHERE creator_id = user_id_;
+    END CASE;
+  END;
+  $block1$ LANGUAGE PLPGSQL;
+
+  RAISE NOTICE '[SETUP]   (SET) FUNCTION: DONE setting up functions that retrieves user password.';
+
+END;
+$block$ LANGUAGE PLPGSQL;
