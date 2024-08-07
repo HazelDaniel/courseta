ac10cf184558af200062d05eca67c69159cfcfd7
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-07 23:38:44 +0100

    updated the script to be context aware

--- db/vacuum.sh
+++ db/vacuum.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 # cleaning up old backup files
 echo "removing old schema backup...";
-for i in $(find ./backup/schema -type f | sort -n | head -n -1); do
+
+if [[ $1 == 'test' ]]; then
+	VACUUM_DIR_PATH='./backup_test';
+else
+	VACUUM_DIR_PATH='./backup';
+fi
+
+for i in $(find $VACUUM_DIR_PATH/schema -type f | sort -n | head -n -1); do
 	echo "removing $i..."
 	rm -rf "$i"
 done &&\
 echo "done!" &&\
 
 echo "removing old tables backup...";
-for i in $(cat ./backup/dependency.txt); do
-	for j in $(find "./backup/tables/$i" -type f | sort -n | head -n -1); do
+for i in $(cat $VACUUM_DIR_PATH/dependency.txt); do
+	for j in $(find "$VACUUM_DIR_PATH/tables/$i" -type f | sort -n | head -n -1); do
 		echo "removing $j..."
 		rm -rf "$j"
 	done
