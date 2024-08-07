c0faad2ccf3dbc09f13064dcd3609ce64bf72632
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-07 23:41:19 +0100

    updated the script to be more context aware

--- db/setup.sh
+++ db/setup.sh
@@ -2,6 +2,11 @@
 # db teardown, cleanup and setup
 source ./.environment.zshrc
 
+if [[ $1 == 'test' ]]; then
+	SETUP_DB='courseta_test';
+else
+	SETUP_DB='courseta';
+fi
 
 for i in $(find ./setup -type f | sort -n); do
 	psql -Utoughware -p5432 -hlocalhost -d postgres -f "$i";
@@ -11,9 +16,7 @@ done
 if [[ $? == 0 ]]; then
 	for i in $(find ./tables -type f | sort -n); do
 		if [[ $? == 0 ]]; then
-			psql -Utoughware -p5432 -hlocalhost -d courseta -f "$i" &&\
-			echo '---------------------------------------------------------------------------------------------------' &&\
-			psql -Utoughware -p5432 -hlocalhost -d courseta_test -f "$i";
+			psql -Utoughware -p5432 -hlocalhost -d $SETUP_DB -f "$i";
 		else
 			break;
 		fi
@@ -23,9 +26,7 @@ fi
 if [[ $? == 0 ]]; then
 	for i in $(find ./functions -type f |  sort -n); do
 		if [[ $? == 0 ]]; then
-			psql -Utoughware -p5432 -hlocalhost -d courseta -f "$i" &&\
-			echo '---------------------------------------------------------------------------------------------------' &&\
-			psql -Utoughware -p5432 -hlocalhost -d courseta_test -f "$i";
+			psql -Utoughware -p5432 -hlocalhost -d $SETUP_DB -f "$i";
 		else
 			break;
 		fi
@@ -35,9 +36,7 @@ fi
 if [[ $? == 0 ]]; then
 	for i in $(find ./triggers -type f |  sort -n); do
 		if [[ $? == 0 ]]; then
-			psql -Utoughware -p5432 -hlocalhost -d courseta -f "$i" &&\
-			echo '---------------------------------------------------------------------------------------------------' &&\
-			psql -Utoughware -p5432 -hlocalhost -d courseta_test -f "$i";
+			psql -Utoughware -p5432 -hlocalhost -d $SETUP_DB -f "$i";
 		else
 			break;
 		fi
@@ -47,9 +46,7 @@ fi
 if [[ $? == 0 ]]; then
 	for i in $(find ./GET-functions -type f |  sort -n); do
 		if [[ $? == 0 ]]; then
-			psql -Utoughware -p5432 -hlocalhost -d courseta -f "$i" &&\
-			echo '---------------------------------------------------------------------------------------------------' &&\
-			psql -Utoughware -p5432 -hlocalhost -d courseta_test -f "$i";
+			psql -Utoughware -p5432 -hlocalhost -d $SETUP_DB -f "$i";
 		else
 			break;
 		fi
@@ -59,9 +56,7 @@ fi
 if [[ $? == 0 ]]; then
 	for i in $(find ./SET-functions -type f |  sort -n); do
 		if [[ $? == 0 ]]; then
-			psql -Utoughware -p5432 -hlocalhost -d courseta -f "$i" &&\
-			echo '---------------------------------------------------------------------------------------------------' &&\
-			psql -Utoughware -p5432 -hlocalhost -d courseta_test -f "$i";
+			psql -Utoughware -p5432 -hlocalhost -d $SETUP_DB -f "$i";
 		else
 			break;
 		fi
