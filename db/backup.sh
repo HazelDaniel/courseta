4e3ec365e43df4e3eba7bd3bca7eb221f966472c
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-07 23:39:28 +0100

    updated the script to handle backups in test database too

--- db/backup.sh
+++ db/backup.sh
@@ -2,18 +2,24 @@
 # backing up db to stored csv/sql files
 
 if [[ $1 == 'test' ]]; then
-	exit 0;
+	BACKUP_DIR_PATH='./backup_test';
+	BACKUP_DB='courseta_test';
+	source ./.environment.zshrc
+else
+	BACKUP_DIR_PATH='./backup';
+	BACKUP_DB='courseta';
+	source ./.environment.zshrc
 fi
-source ./.environment.zshrc
-pg_dump -v -Utoughware -hlocalhost -dcourseta --schema=courseta --schema-only > ./backup/schema/"$(date +%y-%m_%d_%H_%M_%S)"-backup.sql
+
+pg_dump -v -Utoughware -hlocalhost -d$BACKUP_DB --schema=courseta --schema-only > $BACKUP_DIR_PATH/schema/"$(date +%y-%m_%d_%H_%M_%S)"-backup.sql
 
 echo "creating backup for all tables...";
-for i in `cat ./backup/dependency.txt`; do
+for i in `cat $BACKUP_DIR_PATH/dependency.txt`; do
 	echo "backing up: $i..."
-	if [[ ! -d "./backup/tables/$i"  ]]; then
-		mkdir ./backup/tables/"$i"
+	if [[ ! -d "$BACKUP_DIR_PATH/tables/$i"  ]]; then
+		mkdir $BACKUP_DIR_PATH/tables/"$i"
 	fi
-	psql -Utoughware -p5432 -hlocalhost -d courseta -c "\COPY $i TO backup/tables/$i/$i-$(date +%y-%m_%d_%H_%M_%S_%N).csv DELIMITER ',' HEADER CSV";
+	psql -Utoughware -p5432 -hlocalhost -d$BACKUP_DB -c "\COPY $i TO $BACKUP_DIR_PATH/tables/$i/$i-$(date +%y-%m_%d_%H_%M_%S_%N).csv DELIMITER ',' HEADER CSV";
 done
 
 echo "backup created successfully!"
