6f399480b41030609f710894187b4efe9b0b94fc
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-07 23:51:39 +0100

    added middlewares for students access control and auth

--- server/api/v1/middlewares/auth.middleware.ts
+++ server/api/v1/middlewares/auth.middleware.ts
@@ -1,8 +1,11 @@
 import type { NextFunction, Request, Response } from "express";
 import { ServerError } from "../../../utils.js";
-import { CreatorSessionUserType } from "../../../types.d";
+import {
+  CreatorSessionUserType,
+  StudentSessionUserType,
+} from "../../../types.d";
 
-export const localProtected = (
+export const creatorsLocalProtected = (
   req: Request,
   res: Response,
   next: NextFunction
@@ -21,6 +24,29 @@ export const creatorIDProtected = (
 ) => {
   const { creator_id: creatorID } = req.params;
   if ((req.user as CreatorSessionUserType).id !== creatorID)
+    next(new ServerError("you are not allowed to access this route!", 403));
+  next();
+};
+
+export const studentsLocalProtected = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.isAuthenticated())
+    next(
+      new ServerError("only authenticated students can access this route", 401)
+    );
+  next();
+};
+
+export const studentIDProtected = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { student_id: studentID } = req.params;
+  if ((req.user as StudentSessionUserType).id !== studentID)
     next(new ServerError("you are not allowed to access this route!", 403));
   next();
 };
