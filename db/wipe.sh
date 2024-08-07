5598a3b52540b848635b03af945d80ffe40d1b58
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-07 23:41:49 +0100

    updated the script to be more context aware

--- db/wipe.sh
+++ db/wipe.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 # cleaning up all backup files
 echo "removing all schema backup...";
-for i in $(find ./backup/schema -type f); do
+
+if [[ $1 == 'test' ]]; then
+	BACKUP_DIR_PATH='./backup_test';
+	source ./.environment.zshrc
+else
+	BACKUP_DIR_PATH='./backup';
+	source ./.environment.zshrc
+fi
+
+for i in $(find $BACKUP_DIR_PATH/schema -type f); do
 	echo "removing $i..."
 	rm -rf "$i"
 done &&\
 echo "done!" &&\
 
 echo "removing all tables backup...";
-for i in $(cat ./backup/dependency.txt); do
-	for j in $(find "./backup/tables/$i" -type f); do
+for i in $(cat $BACKUP_DIR_PATH/dependency.txt); do
+	for j in $(find "$BACKUP_DIR_PATH/tables/$i" -type f); do
 		echo "removing $j..."
 		rm -rf "$j"
 	done
