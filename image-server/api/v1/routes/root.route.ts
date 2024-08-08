a81583f813aa63a109759b73feef386150cf306f
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-09 00:54:58 +0100

    added the root route

---
+++ image-server/api/v1/routes/root.route.ts
@@ -0,0 +1,23 @@
+import { ErrorBoundary } from "../middlewares/error.middleware.js";
+import express, { NextFunction, Request, Response } from "express";
+
+export const v1Router = express.Router();
+
+v1Router.use(async (req, res, next) => {
+  console.log(`[${req.method}]: ${req.baseUrl}${req.url}`);
+  next();
+});
+
+v1Router.get("/", async (req, res, next) => {
+  try {
+    return res.status(200).json({ message: "welcome" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+v1Router.get("/*", (req, res) => {
+  return res.status(404).json({ message: "endpoint not found" });
+});
+
+v1Router.use(ErrorBoundary);
