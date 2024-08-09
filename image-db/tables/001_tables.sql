17e111991b69726c4fa2d667db251430e1a888fe
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-10 00:52:48 +0100

    namespaced the table under a schema

--- image-db/tables/001_tables.sql
+++ image-db/tables/001_tables.sql
@@ -1,9 +1,10 @@
 DROP SCHEMA IF EXISTS courseta_image CASCADE;
 CREATE SCHEMA courseta_image;
 ALTER DATABASE courseta_image SET search_path TO courseta_image;
+ALTER DATABASE courseta_image_test SET search_path TO courseta_image;
 CREATE EXTENSION IF NOT EXISTS pgcrypto SCHEMA courseta_image;
 
-CREATE TABLE IF NOT EXISTS images (
+CREATE TABLE IF NOT EXISTS courseta_image.images (
 	image_id	UUID NOT NULL PRIMARY KEY,
 	image_url BYTEA DEFAULT '',
 	created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
