3610af8443e9c3582525ce8acee9e2eae40c42e6
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-10 00:54:08 +0100

    corrected the schema and the input parameter types

--- image-db/functions/001_create_image.sql
+++ image-db/functions/001_create_image.sql
@@ -1,15 +1,15 @@
 DO
 $block1$
 BEGIN
-	CREATE OR REPLACE FUNCTION create_image(p_image_url BYTEA, p_image_id UUID)
+	CREATE OR REPLACE FUNCTION create_image(p_image_url TEXT, p_image_id UUID)
 	RETURNS UUID AS $$
 	DECLARE
 		new_image_id 				UUID;
 	BEGIN
-			INSERT INTO courseta_images.images (image_id, image_url)
-			VALUES (p_image_id, p_image_url)
+			INSERT INTO courseta_image.images (image_id, image_url)
+			VALUES (p_image_id, decode(p_image_url, 'base64'))
 			RETURNING images.image_id INTO new_image_id;
-			
+
 			RETURN new_image_id;
 	END;
 	$$ LANGUAGE plpgsql;
