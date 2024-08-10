08f3eb809aec15c7363da1f6c2f50921b95ea151
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-10 01:01:29 +0100

    configured the endpoint of the image server

--- server/api/v1/config.ts
+++ server/api/v1/config.ts
@@ -1,4 +1,6 @@
 import { ConfigOption } from "../../types";
+import { config } from "dotenv";
+config({path: [".env"]});
 
 
 export const v1Config: ConfigOption = {
@@ -9,6 +11,9 @@ export const v1Config: ConfigOption = {
     keyLength: 64,
     saltByteCount: 10,
   },
+  serverOptions: {
+    imageServerBaseUrl: `http://localhost:${process.env.IMAGE_SERVER_PORT}/api/v1/images`
+  }
 };
 
 export default v1Config;
