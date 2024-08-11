aa8e4a42ae43d6e19188b24d827d53afb4e0c34c
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-11 19:06:18 +0100

    updated the result set returned by the function

--- db/GET-functions/get_course_outline.sql
+++ db/GET-functions/get_course_outline.sql
@@ -12,14 +12,20 @@ BEGIN
     total_duration SMALLINT,
     quiz_id UUID,
     total_points INT,
-    quiz_title TEXT
+    quiz_title TEXT,
+    content_title TEXT,
+    content_type courseta.LESSON_CONTENT_TYPE
   ) AS
   $block1$
   BEGIN
     RETURN QUERY SELECT lessons.lesson_id, lessons.title, lessons.course_id,
-    lessons.content_count, lessons.total_duration, quizzes.quiz_id, quizzes.total_points, quizzes.quiz_title
+    lessons.content_count, lessons.total_duration, quizzes.quiz_id, quizzes.total_points, quizzes.quiz_title,
+    lesson_contents.title content_title,
+    lesson_contents.content_type
+
     FROM courseta.lessons
     LEFT JOIN courseta.quizzes USING (lesson_id)
+    LEFT JOIN courseta.lesson_contents USING (lesson_id)
     WHERE lessons.course_id = course_id_;
   END;
   $block1$ LANGUAGE PLPGSQL;
