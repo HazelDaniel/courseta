58abdcad9e35c615afae2a31762126c8650dd6d4
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-11 19:13:36 +0100

    updated the delete condition

--- db/SET-functions/remove_content_from_lesson.sql
+++ db/SET-functions/remove_content_from_lesson.sql
@@ -3,17 +3,16 @@ $block$
 BEGIN
   RAISE NOTICE '[SETUP]   (SET) FUNCTION: setting up the remove_content_from_lesson function ...';
 
-  CREATE OR REPLACE PROCEDURE remove_content_from_lesson (lesson_id_ BIGINT)
+  CREATE OR REPLACE PROCEDURE remove_content_from_lesson (lesson_id_ BIGINT, content_id_ BIGINT)
   LANGUAGE PLPGSQL AS
   $block1$
   BEGIN
-    DELETE from courseta.lesson_contents WHERE lesson_contents.lesson_id = lesson_id_;
+    DELETE from courseta.lesson_contents
+    WHERE lesson_contents.lesson_id = lesson_id_
+    AND lesson_contents.lesson_content_id = content_id_;
   END;
   $block1$;
 
   RAISE NOTICE '[SETUP]   (SET) FUNCTION: DONE setting up the remove_content_from_lesson function.';
 END
 $block$ LANGUAGE PLPGSQL;
-
-
-
