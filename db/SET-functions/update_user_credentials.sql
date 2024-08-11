fc639231dd04d6c21dd374cbf9058beb8cab0313
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-11 19:21:09 +0100

    updated the parameters received by the function

--- db/SET-functions/update_user_credentials.sql
+++ db/SET-functions/update_user_credentials.sql
@@ -16,13 +16,13 @@ BEGIN
   BEGIN
     CASE type_
     WHEN 'student' THEN
-      RETURN QUERY 
+      RETURN QUERY
       UPDATE courseta.students
       SET first_name = COALESCE(first_name_, students.first_name), last_name = COALESCE(last_name_, students.last_name)
       WHERE student_id = user_id_
       RETURNING students.first_name, students.last_name;
     WHEN 'creator' THEN
-      RETURN QUERY 
+      RETURN QUERY
       UPDATE courseta.creators
       SET first_name = COALESCE(first_name_, creators.first_name), last_name = COALESCE(last_name_, creators.last_name)
       WHERE creator_id = user_id_
@@ -38,8 +38,8 @@ BEGIN
 
   CREATE OR REPLACE FUNCTION update_user_password (
     user_id_ UUID,
-    old_password TEXT,
     new_password TEXT,
+		new_salt TEXT,
     type_ courseta.USER_ROLE_TYPE
   )
   RETURNS VOID AS
@@ -48,11 +48,11 @@ BEGIN
     CASE type_
     WHEN 'student' THEN
       UPDATE courseta.students
-      SET password = new_password
+      SET password = new_password, salt = new_salt
       WHERE student_id = user_id_;
     WHEN 'creator' THEN
       UPDATE courseta.creators
-      SET password = new_password
+      SET password = new_password, salt = new_salt
       WHERE creator_id = user_id_;
     END CASE;
   END;
@@ -92,6 +92,7 @@ BEGIN
   CREATE OR REPLACE FUNCTION update_user_avatar (
     user_id_ UUID,
     new_avatar TEXT,
+    new_mime_type TEXT,
     type_ courseta.USER_ROLE_TYPE
   )
   RETURNS VOID AS
@@ -100,11 +101,13 @@ BEGIN
     CASE type_
     WHEN 'student' THEN
       UPDATE courseta.students
-      SET avatar = avatar || ('{"url": "' || new_avatar || '", "created_at": "' ||  students.created_at::TEXT || '", "updated_at": "' || CURRENT_TIMESTAMP::TEXT || '"}')::JSONB
+      SET avatar_meta = students.avatar_meta || ('{"created_at": "' ||  students.created_at::TEXT || '", "updated_at": "' || CURRENT_TIMESTAMP::TEXT || '", "id": "' || (COALESCE(students.avatar_meta->>'id', gen_random_uuid()::TEXT))::TEXT || '", "mime_type": "' || new_mime_type::TEXT || '"}')::JSONB,
+      avatar = COALESCE(decode(new_avatar, 'base64'), avatar)
       WHERE student_id = user_id_;
     WHEN 'creator' THEN
       UPDATE courseta.creators
-      SET avatar = avatar || ('{"url": "' || new_avatar || '", "created_at": "' ||  creators.created_at::TEXT || '", "updated_at": "' || CURRENT_TIMESTAMP::TEXT || '"}')::JSONB
+      SET avatar_meta = creators.avatar_meta || ('{"created_at": "' ||  creators.created_at::TEXT || '", "updated_at": "' || CURRENT_TIMESTAMP::TEXT || '", "id": "' || (COALESCE(creators.avatar_meta->>'id', gen_random_uuid()::TEXT))::TEXT || '", "mime_type": "' || new_mime_type::TEXT || '"}')::JSONB,
+      avatar = COALESCE(decode(new_avatar, 'base64'), avatar)
       WHERE creator_id = user_id_;
     END CASE;
   END;
