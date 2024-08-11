e1f216b10b4182e30e16f3540efc8945585a4537
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-11 19:23:45 +0100

    updated the setup script for the server auth session store

--- db/setup/002_setup.sql
+++ db/setup/002_setup.sql
@@ -1,5 +1,8 @@
 DROP DATABASE IF EXISTS courseta_users_session;
 CREATE DATABASE courseta_users_session;
 
-DROP DATABASE IF EXISTS courseta_users_session_test;
-CREATE DATABASE courseta_users_session_test;
+DROP DATABASE IF EXISTS courseta_creators_session_test;
+CREATE DATABASE courseta_creators_session_test;
+
+DROP DATABASE IF EXISTS courseta_students_session_test;
+CREATE DATABASE courseta_students_session_test;
