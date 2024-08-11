3772e74ab075f821dda94aaae6001ac32bb9cb8b
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-11 19:08:12 +0100

    updated the column values returned by the function

--- db/GET-functions/get_current_creator.sql
+++ db/GET-functions/get_current_creator.sql
@@ -4,24 +4,24 @@ BEGIN
   RAISE NOTICE '[SETUP]   (GET) FUNCTION: setting up the get_current_creator function ...';
 
   CREATE OR REPLACE FUNCTION get_current_creator (email_ TEXT) RETURNS
-  TABLE (creator_id UUID, email VARCHAR, role USER_ROLE_TYPE, creator_pass TEXT,
-  avatar_url TEXT, first_name VARCHAR, last_name VARCHAR, course_review_count INT,
-  student_count INT, course_count INT, average_course_rating NUMERIC) AS
+  TABLE (creator_id UUID, email VARCHAR, role USER_ROLE_TYPE,
+  avatar TEXT, avatar_meta JSONB, first_name VARCHAR, last_name VARCHAR, course_review_count INT,
+  student_count INT, course_count INT, average_course_rating NUMERIC, created_at TIMESTAMPTZ) AS
   $block1$
   BEGIN
-    RETURN QUERY SELECT creators.creator_id, creators.email, creators.role, creators.creator_pass, creators.avatar->>'url' avatar_url,
+    RETURN QUERY SELECT creators.creator_id, creators.email, creators.role, translate(encode(creators.avatar, 'base64'), E' \t\n\r', ''), creators.avatar_meta,
     creators.first_name, creators.last_name, creators.course_review_count, creators.student_count, creators.course_count,
-    creators.average_course_rating
+    creators.average_course_rating, creators.created_at
     FROM creators
     WHERE creators.email = email_;
   END;
   $block1$ LANGUAGE PLPGSQL;
 
   CREATE OR REPLACE FUNCTION get_current_creator_validate (email_ TEXT) RETURNS
-  TABLE (creator_id UUID, creator_pass TEXT, password TEXT) AS
+  TABLE (creator_id UUID, creator_pass TEXT, password TEXT, salt TEXT) AS
   $block1$
   BEGIN
-    RETURN QUERY SELECT creators.creator_id, creators.creator_pass, creators.password
+    RETURN QUERY SELECT creators.creator_id, creators.creator_pass, creators.password, creators.salt
     FROM creators
     WHERE creators.email = email_;
   END;
