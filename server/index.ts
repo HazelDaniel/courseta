bccd776fe58d08fc9ec3f1d9d7a67961c823bd4b
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-14 18:16:00 +0100

    overwrote the default payload limit for the server and leveraged env files to listen dynamically on ports

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import { config } from "dotenv";
 config();
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = process.env.PORT || process.env.SERVER_PORT || 3000;
 import { v1Router } from "./api/v1/routes/root.route.js";
 import cors from "cors";
 import helmet from "helmet";
@@ -10,7 +10,7 @@ import helmet from "helmet";
 const app = express();
 app.use(cors());
 app.use(helmet());
-app.use(express.json());
+app.use(express.json({ limit: "30MB" }));
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", v1Router);
 
