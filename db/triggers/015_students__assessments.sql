852c583f88f624ba01144016551aea8d3a70f56a
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-20 11:07:22 +0100

    added a script for all event triggers that happen on the students__assessments table

---
+++ db/triggers/015_students__assessments.sql
@@ -0,0 +1,20 @@
+DO
+$block$
+BEGIN
+  -- RAISE NOTICE '[SETUP]   TRIGGER: updating students.points update triggers...';
+
+  CREATE OR REPLACE TRIGGER trigger_0c_assessment_points_update
+  AFTER INSERT
+  ON students__assessments
+  FOR EACH ROW
+  EXECUTE FUNCTION p_03_run_submission_effect();
+
+  CREATE OR REPLACE TRIGGER trigger_0b_student_points_recalc
+  BEFORE DELETE
+  ON students__assessments
+  FOR EACH ROW
+  EXECUTE FUNCTION p_03_agg_re_calculate_student_point();
+
+-- RAISE NOTICE '[SETUP]   TRIGGER: updating students.points update triggers...';
+END;
+$block$ LANGUAGE PLPGSQL;
