6bd815bff28524e002c006158b15ab750ef1c219
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-22 19:44:10 +0100

    replace trigger tables, and trigger operations, added new triggers

--- db/triggers/006_trigger_students__questions_points_accumulated_update.sql
+++ db/triggers/006_trigger_students__questions_points_accumulated_update.sql
@@ -2,12 +2,18 @@ DO
 $block$
 BEGIN
   -- RAISE NOTICE '[SETUP]   TRIGGER: updating students__questions.points_accumulated update triggers...';
+
   CREATE OR REPLACE TRIGGER trigger_students__questions_points_accumulated_update
   AFTER INSERT
-  ON students__answers
+  ON students__questions
   FOR EACH ROW
   EXECUTE FUNCTION p_02_agg_answer_submission_points_to_question();
 
+  CREATE OR REPLACE TRIGGER trigger_students__answer_points_accumulation
+  AFTER INSERT
+  ON students__answers
+  FOR EACH ROW
+  EXECUTE FUNCTION p_02_agg_answer_submission_points();
   -- RAISE NOTICE '[SETUP]   TRIGGER: DONE updating students__questions.points_accumulated update triggers...';
 END
 $block$ LANGUAGE PLPGSQL;
