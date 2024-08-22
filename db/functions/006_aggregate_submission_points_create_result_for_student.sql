acaa4f7350d076954fb3adc7be01d7b98e7d1932
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-22 19:32:37 +0100

    added logic to deduct student's existing points on an assessment submission, added logic to deduct student's points on an assessment deletion, added some views for debugging purposes

--- db/functions/006_aggregate_submission_points_create_result_for_student.sql
+++ db/functions/006_aggregate_submission_points_create_result_for_student.sql
@@ -3,52 +3,103 @@ $block$
 BEGIN
   -- RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for update on the students.points column.';
 
-  CREATE OR REPLACE FUNCTION p_02_deduct_student_existing_points (student_id_ UUID, assessment_id_ UUID, submitted_at_ TIMESTAMPTZ) RETURNS VOID AS
+  CREATE OR REPLACE FUNCTION p_02_deduct_student_existing_points (student_id_ UUID, assessment_id_ UUID, submitted_at_ TIMESTAMPTZ)
+  RETURNS VOID AS
   $block1$
   DECLARE
     retake_points          INT DEFAULT 0;
   BEGIN
-    SELECT INTO retake_points total_points_accumulated
-    FROM students__assessments
-		WHERE students__assessments.student_id = student_id_
-		AND students__assessments.assessment_id = assessment_id_
-    AND students__assessments.submitted_at <> submitted_at_
-    ORDER BY submitted_at DESC
-    LIMIT 1;
+    WITH retakes AS (
+      SELECT total_points_accumulated,
+      ROW_NUMBER() OVER (PARTITION BY assessment_id ORDER BY submitted_at DESC) retake_number
+      FROM students__assessments
+      WHERE students__assessments.student_id = student_id_
+  		AND students__assessments.assessment_id = assessment_id_
+    )
+    SELECT INTO retake_points COALESCE(SUM(total_points_accumulated), 0) FROM retakes
+    WHERE retake_number = 2;
 
-    retake_points = COALESCE(retake_points, 0);
-    -- RAISE NOTICE '[debug]: retake points is %', retake_points;
-
+    -- RAISE EXCEPTION 'retake points is %', retake_points;
 
     UPDATE students SET points = (points - retake_points)
     WHERE students.student_id = student_id_; -- deducting existing retake points from the students points
   END;
   $block1$ LANGUAGE PLPGSQL;
 
-    -- TODO: this should be triggered once or always re-computed otherwise
-  CREATE OR REPLACE FUNCTION p_02_agg_assessment_submission_points_to_student () RETURNS TRIGGER AS
+
+
+  CREATE OR REPLACE PROCEDURE p_02_deduct_student_last_points_on (student_id_ UUID, assessment_id_ UUID, submitted_at_ TIMESTAMPTZ)
+  LANGUAGE PLPGSQL AS
   $block1$
   DECLARE
-    gen_result_id                   UUID;
+    last_points           INT DEFAULT 0;
+    debug_student_points_ INT DEFAULT 0;
+    last_attempt                 RECORD;
   BEGIN
-    IF NEW.waiting = OLD.waiting OR NEW.waiting = 'true' THEN
-      RETURN NEW; -- don't do anything further if the assessment submission is not past the waiting state
+
+    SELECT INTO last_attempt se.submitted_at, se.assessment_id
+    FROM students__assessments se
+    WHERE se.assessment_id = assessment_id_
+    AND se.student_id = student_id_
+    ORDER BY submitted_at DESC;
+
+    IF last_attempt.submitted_at <> submitted_at_ OR last_attempt.assessment_id <> assessment_id_ THEN
+      RETURN; -- we are basically working with last submissions of all quizzes
     END IF;
 
-    PERFORM p_02_deduct_student_existing_points(NEW.student_id, NEW.assessment_id, NEW.submitted_at);
+    SELECT INTO last_points COALESCE(total_points_accumulated, 0)
+    FROM students__assessments
+    WHERE students__assessments.student_id = student_id_
+  	AND students__assessments.assessment_id = assessment_id_
+  	ORDER BY submitted_at DESC LIMIT 1;
 
-    -- RAISE NOTICE '[debug]: WE ARE AGGREGATING STUDENT POINTS FROM ASSESSMENT SUBMISSION';
+  	SELECT INTO debug_student_points_ points
+  	FROM courseta.students
+  	WHERE students.student_id = student_id_; -- deducting existing retake points from the students points
 
-    UPDATE students SET points = points + NEW.total_points_accumulated
-		WHERE students.student_id = NEW.student_id;
-    -- we should create a corresponding assessment result here
+  	RAISE NOTICE 'last points is , %, while student point is %', last_points, debug_student_points_;
+  	-- RAISE EXCEPTION 'last points is , %', last_points;
 
-    gen_result_id := p_02_upsert_equiv_assessment_result(NEW.student_id, NEW.assessment_id, NEW.submitted_at);
-    CALL p_01_update_score_on_assessment_result(NEW.student_id, NEW.assessment_id, NEW.submitted_at, gen_result_id);
+    UPDATE students SET points = (points - last_points)
+    WHERE students.student_id = student_id_; -- deducting existing retake points from the students points
+  END;
+  $block1$;
 
-    RETURN NEW;
+
+  CREATE OR REPLACE FUNCTION p_03_agg_re_calculate_student_point () RETURNS TRIGGER AS
+  $block1$
+  BEGIN
+    CALL p_02_deduct_student_last_points_on(OLD.student_id, OLD.assessment_id, OLD.submitted_at);
+    RETURN OLD;
   END;
   $block1$ LANGUAGE PLPGSQL;
+
+
+  CREATE OR REPLACE FUNCTION p_04_agg_re_calc_course_progress_using_quiz () RETURNS TRIGGER AS
+  $block1$
+  DECLARE
+  submission_entry        RECORD;
+  BEGIN
+    FOR submission_entry IN (
+    WITH all_last_attempts_on AS (
+    SELECT ROW_NUMBER() OVER (PARTITION BY assessment_id ORDER BY submitted_at DESC),
+    assessment_id, student_id, submitted_at FROM students__assessments
+    JOIN quizzes USING (assessment_id) WHERE quiz_id = OLD.quiz_id
+    )
+    SELECT assessment_id, student_id, submitted_at FROM all_last_attempts_on
+    WHERE ROW_NUMBER = 1) LOOP
+      RAISE NOTICE 'submission entry is %', submission_entry;
+      CALL p_01_deduct_course_progress_from_assessment(submission_entry.assessment_id, submission_entry.student_id, submission_entry.submitted_at);
+    END LOOP;
+    RETURN OLD;
+  END;
+  $block1$ LANGUAGE PLPGSQL;
+
+
+------------------------------------ DEBUGGING---------------------------------------------
+  CREATE OR REPLACE VIEW debug_recent_submission_points as (
+  SELECT ROW_NUMBER () OVER (PARTITION BY assessment_id ORDER BY submitted_at DESC), total_points_accumulated, assessment_id FROM students__assessments
+  );
 
   -- RAISE NOTICE '[SETUP]  PROCEDURE/FUNCTION: DONE setting up procedures/functions for update on the students.points column.';
 
