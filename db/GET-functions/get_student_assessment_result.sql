2dd2afd787289278239d276e5fa88645eb0a55aa
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-22 19:27:32 +0100

    fixed the output of the query

--- db/GET-functions/get_student_assessment_result.sql
+++ db/GET-functions/get_student_assessment_result.sql
@@ -9,8 +9,10 @@ RAISE NOTICE '[SETUP]   (GET) FUNCTION: setting up function that gets assessment
   $block2$
   DECLARE
     result    JSONB;
+    quizzes_  JSONB;
+    exams_    JSONB;
   BEGIN
-    RETURN (SELECT json_agg(json_build_object(
+    SELECT INTO quizzes_ json_agg(json_build_object(
       'status', qar.status,
       'dateAttempted', qar.attempted_at,
       'percentScore', qar.score,
@@ -19,10 +21,9 @@ RAISE NOTICE '[SETUP]   (GET) FUNCTION: setting up function that gets assessment
     FROM courseta.assessments_results qar
     JOIN courseta.quizzes q USING (assessment_id)
     WHERE qar.student_id = student_id_
-    GROUP BY qar.student_id
-    UNION ALL -- would have sticked to just UNION but all rows in the quizzes table
-    -- will be distinct from that in the exam table so, perf benefit
-    SELECT json_agg(json_build_object(
+    GROUP BY qar.student_id;
+
+    SELECT INTO exams_ json_agg(json_build_object(
       'status', ear.status,
       'dateAttempted', ear.attempted_at,
       'percentScore', ear.score,
@@ -31,7 +32,10 @@ RAISE NOTICE '[SETUP]   (GET) FUNCTION: setting up function that gets assessment
     FROM courseta.assessments_results ear
     JOIN courseta.exams e USING (assessment_id)
     WHERE ear.student_id = student_id_
-    GROUP BY ear.student_id);
+    GROUP BY ear.student_id;
+
+    result := json_build_object('exams', exams_, 'quizzes', quizzes_);
+    RETURN result;
   END;
   $block2$ LANGUAGE PLPGSQL;
 
