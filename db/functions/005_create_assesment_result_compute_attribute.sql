e44aab6c58258bb3e3bffe9b1aea857b35f4af56
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-22 19:30:41 +0100

    handled null cases, added a logic to update the status field

--- db/functions/005_create_assesment_result_compute_attribute.sql
+++ db/functions/005_create_assesment_result_compute_attribute.sql
@@ -23,18 +23,18 @@ BEGIN
 		SELECT INTO equiv_assessment quizzes.pass_score, quizzes.total_points FROM courseta.quizzes
     WHERE quiz_id = assessment_id_;
 
-    IF equiv_assessment IS NULL THEN
+    IF NOT FOUND THEN
       SELECT INTO equiv_assessment exams.pass_score, exams.total_points FROM courseta.exams
       WHERE exam_id = assessment_id_;
     END IF;
 
     -- RAISE NOTICE '[debug]: accumulated points / total points = % / %', acc_points::NUMERIC , tot_points::NUMERIC;
 
-    res := ((acc_points::NUMERIC / equiv_assessment.total_points::NUMERIC) * 100);
+    res := COALESCE(((acc_points::NUMERIC / equiv_assessment.total_points::NUMERIC) * 100), 0);
 
     UPDATE assessments_results
     SET score = res,
-    status = (CASE WHEN res < equiv_assessment.pass_score THEN 'failed' ELSE 'passed' END)
+    status = (CASE WHEN res < equiv_assessment.pass_score THEN 'failed'::courseta.ASSESSMENT_RESULT_STATUS_TYPE ELSE 'passed'::courseta.ASSESSMENT_RESULT_STATUS_TYPE END)
 		WHERE attempted_at = time_attempted
     AND assessments_results.student_id = student_id_ AND assessments_results.assessment_id = assessment_id_;
   END;
@@ -45,7 +45,7 @@ BEGIN
   RAISE NOTICE '<[SETUP]  PROCEDURE/FUNCTION: setting up procedures/functions for creation of assessment_result.';
 
   CREATE OR REPLACE FUNCTION p_02_upsert_equiv_assessment_result
-  (student_id_ UUID, assessment_id_ UUID, time_attempted TIMESTAMPTZ)
+  (student_id_ UUID, assessment_id_ UUID, time_attempted TIMESTAMPTZ, status_ courseta.ASSESSMENT_RESULT_STATUS_TYPE)
   RETURNS UUID
   AS
   $block2$
@@ -53,8 +53,8 @@ BEGIN
     res               UUID;
     equiv_pass_score   INT;
   BEGIN
-    INSERT INTO assessments_results (student_id, assessment_id, attempted_at, score)
-    VALUES (student_id_, assessment_id_, time_attempted, 0)
+    INSERT INTO assessments_results (student_id, assessment_id, attempted_at, score, status)
+    VALUES (student_id_, assessment_id_, time_attempted, 0, status_::courseta.ASSESSMENT_RESULT_STATUS_TYPE)
     ON CONFLICT DO NOTHING
     RETURNING assessment_result_id INTO res;
 
