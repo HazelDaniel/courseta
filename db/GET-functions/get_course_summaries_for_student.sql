e282ba6ae5295abce9fda9b533b56fa3ae28f4b7
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-22 19:47:35 +0100

    filtered out the archived courses for display

--- db/GET-functions/get_course_summaries_for_student.sql
+++ db/GET-functions/get_course_summaries_for_student.sql
@@ -17,7 +17,9 @@ BEGIN
     RETURN QUERY SELECT courses.title, courses.lesson_count, translate(encode(courses.avatar, 'base64'), E' \t\n\r', ''), courses.avatar_meta,
     students__courses.progress, courses.course_id
     FROM courseta.students__courses
-    JOIN courseta.courses USING (course_id) WHERE students__courses.student_id = student_id_;
+    JOIN courseta.courses USING (course_id)
+    WHERE students__courses.student_id = student_id_
+    AND courses.archived = 'false';
   END;
   $block1$ LANGUAGE PLPGSQL;
 
