e36d1812981adf85d21a652bbde16f29fa1f3bf3
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-30 17:13:49 +0100

    updated the functions to return the user id on insertion

--- db/SET-functions/set_new_creator.sql
+++ db/SET-functions/set_new_creator.sql
@@ -9,50 +9,38 @@ BEGIN
     first_name_ VARCHAR,
     last_name_ VARCHAR,
     password_ TEXT,
-    -- avatar_url TEXT,
-		-- avatar_id UUID,
-    -- mime_type TEXT,
-    salt_ TEXT
-  ) RETURNS VOID
+    salt_ TEXT,
+    verification_id_ TEXT
+  ) RETURNS UUID
   AS
   $block1$
+  DECLARE
+    result_id   UUID;
   BEGIN
-    -- RETURN QUERY
-    -- INSERT INTO creators (email, first_name, last_name, password, salt, avatar, avatar_meta, created_at)
-    -- VALUES (email_, first_name_, last_name_, password_, salt_,
-    -- decode(avatar_url, 'base64'),
-    -- ('{"created_at": "' ||  CURRENT_TIMESTAMP::TEXT || '", "updated_at" : "' || CURRENT_TIMESTAMP::TEXT || '", "id": "' || avatar_id ||  '", "mime_type": "' || mime_type::TEXT || '"}')::JSONB,
-    -- CURRENT_TIMESTAMP)
-    INSERT INTO creators (email, first_name, last_name, password, salt, created_at)
-    VALUES (email_, first_name_, last_name_, password_, salt_, CURRENT_TIMESTAMP);
+    INSERT INTO creators (email, first_name, last_name, password, salt, created_at, verification_id)
+    VALUES (email_, first_name_, last_name_, password_, salt_, CURRENT_TIMESTAMP, verification_id_)
+    RETURNING creator_id INTO result_id;
+
+    RETURN result_id;
   END;
   $block1$ LANGUAGE PLPGSQL;
 
-  CREATE OR REPLACE FUNCTION set_new_creator (
-  -- [OVERLOAD]: for testing
-    email_ VARCHAR,
-    first_name_ VARCHAR,
-    last_name_ VARCHAR,
-    password_ TEXT,
-    -- avatar_url TEXT,
-		-- avatar_id UUID,
-    -- mime_type TEXT,
-    creator_id_ UUID,
-    salt_ TEXT
-  ) RETURNS VOID
-  AS
-  $block1$
-  BEGIN
-    -- RETURN QUERY 
-    -- INSERT INTO creators (email, first_name, last_name, password, salt, avatar, avatar_meta, created_at, creator_id)
-    -- VALUES (email_, first_name_, last_name_, password_, salt_,
-    -- decode(avatar_url, 'base64'),
-    -- ('{"created_at": "' ||  CURRENT_TIMESTAMP::TEXT || '", "updated_at" : "' || CURRENT_TIMESTAMP::TEXT || '", "id": "' || avatar_id ||  '", "mime_type": "' || mime_type::TEXT || '"}')::JSONB,
-    -- CURRENT_TIMESTAMP, creator_id_)
-    INSERT INTO creators (email, first_name, last_name, password, salt, created_at, creator_id)
-    VALUES (email_, first_name_, last_name_, password_, salt_, CURRENT_TIMESTAMP, creator_id_);
-  END;
-  $block1$ LANGUAGE PLPGSQL;
+  -- CREATE OR REPLACE FUNCTION set_new_creator (
+  -- -- [OVERLOAD]: for testing
+  --   email_ VARCHAR,
+  --   first_name_ VARCHAR,
+  --   last_name_ VARCHAR,
+  --   password_ TEXT,
+  --   creator_id_ UUID,
+  --   salt_ TEXT
+  -- ) RETURNS VOID
+  -- AS
+  -- $block1$
+  -- BEGIN
+  --   INSERT INTO creators (email, first_name, last_name, password, salt, created_at, creator_id)
+  --   VALUES (email_, first_name_, last_name_, password_, salt_, CURRENT_TIMESTAMP, creator_id_);
+  -- END;
+  -- $block1$ LANGUAGE PLPGSQL;
 
   RAISE NOTICE '[SETUP]   (SET) FUNCTION: DONE setting up the set_new_creator function.';
 END;
