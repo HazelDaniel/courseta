15267c99049bac6cee260269eca0ff8584ddd02c
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-30 16:50:29 +0100

    added the first job: a job that removes unverified users within a time window from the db

---
+++ server/api/v1/jobs/vacuum-users.job.ts
@@ -0,0 +1,33 @@
+import { pool } from "../../../db.js";
+import type { QueryConfig } from "pg";
+import { log } from "../../../utils.js";
+import cron from "node-cron";
+import chalk from "chalk";
+
+const vacuumUsers = async () => {
+  const client = await pool.connect();
+  try {
+    log("running job: ", chalk.bgBlue("VACUUM_UNVERFIED_USERS"), " ...");
+    const query: QueryConfig<void> = {
+      name: "vacuum_unverified_users",
+      text: "SELECT vacuum_unverified_users()",
+    };
+    await client.query(query);
+  } catch (err) {
+    log(
+      "could not perform job: ",
+      chalk.bgBlue("VACUUM_UNVERFIED_USERS"),
+      " !",
+      "reason : ",
+      err instanceof Error ? err.message : "NULL"
+    );
+  } finally {
+    client.release();
+  }
+};
+
+export const initJob = () => {
+  const scheduleString =
+    process.env.CST_CONTEXT === "test" ? "* * * * *" : "* 9,21 * * *";
+  cron.schedule(scheduleString, vacuumUsers);
+};
