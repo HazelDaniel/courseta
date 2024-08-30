d55e7b34359815ffac254b6d5a00cbc1c3c3ab4c
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-30 17:07:12 +0100

    added more config parameters

--- server/api/v1/config.ts
+++ server/api/v1/config.ts
@@ -1,6 +1,6 @@
 import { ConfigOption } from "../../types";
 import { config } from "dotenv";
-config({ path: [".env", ".env.dev"] });
+config({ path: [".env", ".env.dev", ".env.prod"] });
 
 export const v1Config: ConfigOption = {
   hashingOptions: {
@@ -13,7 +13,12 @@ export const v1Config: ConfigOption = {
   serverOptions: {
     imageServerBaseUrl: `http://localhost:${process.env.IMAGE_SERVER_PORT}/api/v1/images`,
     debugMode: !!(process.env.CST_CONTEXT === "test"),
+    jwtSecret: (process.env.CST_JWT_SECRET as string),
+    clientURL: process.env.CST_CONTEXT === "test" ? `${process.env.CST_CLIENT_URL_DEV}` : `${process.env.CST_CLIENT_URL_PROD}`
   },
+  serviceOptions: {
+    platformEmail: `${process.env.CST_PLATFORM_EMAIL}`
+  }
 };
 
 export default v1Config;
